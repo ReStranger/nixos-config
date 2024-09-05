b07a66c3a6efb5d4c078329f86bff0fa804a3a94
ReStranger <strengerplayr@github.com>
2024-09-05 23:23:35 +0300

    hotfix: xdg-user-dirs module

--- home/restranger/configs/xdg-user-dirs/default.nix
+++ home/restranger/configs/xdg-user-dirs/default.nix
@@ -1,15 +1,6 @@
 {
-  environment.etc = {
-    "xdg/user-dirs.defaults".text = ''
-      XDG_DOCUMENTS_DIR="$HOME/Documents"
-      XDG_MUSIC_DIR="$HOME/Music"
-      XDG_PICTURES_DIR="$HOME/Pictures"
-      XDG_VIDEOS_DIR="$HOME/Videos"
-      XDG_DOWNLOAD_DIR="$HOME/Downloads"
-      XDG_DESKTOP_DIR="$HOME/"
-      XDG_TEMPLATES_DIR="$HOME/"
-      XDG_PUBLICSHARE_DIR="$HOME/"
-    '';
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
   };
-
 }
