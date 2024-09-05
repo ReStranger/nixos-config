24ae801773c0f4de4763eee0b688f11bc45688df
ReStranger <strengerplayr@gmail.com>
2024-09-06 00:56:01 +0300

    fix: theme module

--- home/restranger/modules/themes/default.nix
+++ home/restranger/modules/themes/default.nix
@@ -1,14 +1,20 @@
 {
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-  };
   gtk = {
     enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome.gnome-themes-extra;
+    iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
+    };
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
     };
   };
 }
