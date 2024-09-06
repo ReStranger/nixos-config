97d93fe68c3668637061f585b63e62d24f03d373
ReStranger <strengerplayr@gmail.com>
2024-09-06 22:27:49 +0300

    fix: hardware config

--- hosts/magicbook/hardware-configuration.nix
+++ hosts/magicbook/hardware-configuration.nix
@@ -2,34 +2,34 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" "amdgpu" ];
-  boot.extraModulePackages = [ ];
-
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/bfda191b-5b77-4a10-a353-8be0599fd3ba";
+    {
+      device = "/dev/disk/by-uuid/bfda191b-5b77-4a10-a353-8be0599fd3ba";
       fsType = "btrfs";
       options = [ "subvol=@" ];
     };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/bfda191b-5b77-4a10-a353-8be0599fd3ba";
+    {
+      device = "/dev/disk/by-uuid/bfda191b-5b77-4a10-a353-8be0599fd3ba";
       fsType = "btrfs";
       options = [ "subvol=@home" ];
     };
 
   fileSystems."/.snapshots" =
-    { device = "/dev/disk/by-uuid/bfda191b-5b77-4a10-a353-8be0599fd3ba";
+    {
+      device = "/dev/disk/by-uuid/bfda191b-5b77-4a10-a353-8be0599fd3ba";
       fsType = "btrfs";
       options = [ "subvol=@.snapshots" ];
     };
 
   fileSystems."/boot/efi" =
-    { device = "/dev/nvme0n1p1";
+    {
+      device = "/dev/nvme0n1p1";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
