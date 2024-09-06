5b466aa2ea9b15643c4e009c3416e4e2cd36568e
ReStranger <strengerplayr@gmail.com>
2024-09-07 00:40:15 +0300

    add: upower config

--- hosts/magicbook/modules/power-management/default.nix
+++ hosts/magicbook/modules/power-management/default.nix
@@ -1,10 +1,13 @@
 {
   services.thermald.enable = true;
   services.upower.enable = true;
+  services.upower.package = pkgs.upower;
+  services.upower.percentageLow = 5;
+  services.upower.percentageCritical = 10;
 
   imports = [
-     #./auto-cpufreq.nix
-     #./tlp.nix
-     ./power-profiles-deamon.nix
+    #./auto-cpufreq.nix
+    #./tlp.nix
+    ./power-profiles-deamon.nix
   ];
 }
