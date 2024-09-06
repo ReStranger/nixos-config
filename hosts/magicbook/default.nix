871086cdbef4d517869af960310e9292fb2383c5
ReStranger <strengerplayr@gmail.com>
2024-09-06 22:27:49 +0300

    fix: hardware config

--- hosts/magicbook/default.nix
+++ hosts/magicbook/default.nix
@@ -14,22 +14,23 @@
   time.timeZone = "Europe/Moscow";
   i18n.defaultLocale = "ru_RU.UTF-8";
   i18n.extraLocaleSettings = {
-    LANG="ru_RU.UTF-8";
-    LC_ADDRESS="ru_RU.UTF-8";
-    LC_IDENTIFICATION="ru_RU.UTF-8";
-    LC_MEASUREMENT="ru_RU.UTF-8";
-    LC_MONETARY="ru_RU.UTF-8";
-    LC_NAME="ru_RU.UTF-8";
-    LC_NUMERIC="ru_RU.UTF-8";
-    LC_PAPER="ru_RU.UTF-8";
-    LC_TELEPHONE="ru_RU.UTF-8";
-    LC_TIME="ru_RU.UTF-8";
+    LANG = "ru_RU.UTF-8";
+    LC_ADDRESS = "ru_RU.UTF-8";
+    LC_IDENTIFICATION = "ru_RU.UTF-8";
+    LC_MEASUREMENT = "ru_RU.UTF-8";
+    LC_MONETARY = "ru_RU.UTF-8";
+    LC_NAME = "ru_RU.UTF-8";
+    LC_NUMERIC = "ru_RU.UTF-8";
+    LC_PAPER = "ru_RU.UTF-8";
+    LC_TELEPHONE = "ru_RU.UTF-8";
+    LC_TIME = "ru_RU.UTF-8";
   };
 
   users.users.restranger = {
     isNormalUser = true;
     shell = pkgs.zsh;
     extraGroups = [ "wheel" "input" "networkmanager" ];
+    hashedPassword = "$6$/k2DxKT/Biwenuzi$rxMb4alvs9KiBsfrI4UIiTWpTNYgvpvq8jGLl1thkTKX00APg6EPt1E9mO6DgpDHrOjwFWlmLwQiLcIk.w4o20";
   };
 
 
