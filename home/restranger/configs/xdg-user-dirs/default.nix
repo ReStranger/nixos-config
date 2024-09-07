b3db253be7e9287c11a0cb03d18cb25cde1d9390
ReStranger <strengerplayr@gmail.com>
2024-09-07 21:20:29 +0300

    upd: xdg-user-dirs config

--- home/restranger/configs/xdg-user-dirs/default.nix
+++ home/restranger/configs/xdg-user-dirs/default.nix
@@ -1,6 +1,15 @@
+{ config, ... }:
 {
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
+    desktop = "${config.home.homeDirectory}/";
+    publicShare = "${config.home.homeDirectory}/";
+  };
+  xdg.terminal-exec = {
+    enable = true;
+    settings = {
+      default = [ "kitty.desktop" ];
+    };
   };
 }
