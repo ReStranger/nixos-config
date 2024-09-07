1d86b4582c8752a0714c49b1b15a3dfd8fd2b0cc
ReStranger <strengerplayr@gmail.com>
2024-09-08 02:14:16 +0300

    fix: cache

--- hosts/magicbook/packages.nix
+++ hosts/magicbook/packages.nix
@@ -33,12 +33,13 @@
     };
   };
   nix = {
-  settings = {
-    substituters = [
-      "https://cache.garnix.io"
-    ];
-    trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    ];
+    settings = {
+      substituters = [
+        "https://cache.garnix.io"
+      ];
+      trusted-public-keys = [
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
+    };
   };
 }
