30a484d6b14eb127e01bd4604ed6fa1221ac3cd4
ReStranger <strengerplayr@gmail.com>
2024-09-08 23:12:16 +0300

    add: ananicy-cpp

--- hosts/pc/modules/services.nix
+++ hosts/pc/modules/services.nix
@@ -1,5 +1,9 @@
 {
   services.gvfs.enable = true;
   systemd.oomd.enable = true;
+  services.ananicy = {
+    enable = true;
+    package = pkgs.ananicy-cpp;
+  };
   services.openssh.enable = true;
 }
