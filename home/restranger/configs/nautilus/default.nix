bfc2850de46013a44d31a9916edba5a81b323773
ReStranger <strengerplayr@gmail.com>
2024-09-10 02:23:47 +0300

    add: nautilus config

--- home/restranger/configs/nautilus/default.nix
+++ home/restranger/configs/nautilus/default.nix
@@ -1,6 +1,24 @@
+{ pkgs, ... }:
+let
+  nautilusEnv = pkgs.buildEnv {
+    name = "nautilus-env";
+
+    paths = with pkgs; [
+      nautilus
+      nautilus-python
+      nautilus-open-any-terminal
+    ];
+  };
+in
+
 {
-  programs.nautilus-open-any-terminal = {
+  home = {
+    packages = [ nautilusEnv ];
+    sessionVariables.NAUTILUS_4_EXTENSION_DIR = "${nautilusEnv}/lib/nautilus/extensions-4";
+  };
+
+  dconf = {
     enable = true;
-    terminal = "kitty";
+    settings."com/github/stunkymonkey/nautilus-open-any-terminal".terminal = "kitty";
   };
 }
