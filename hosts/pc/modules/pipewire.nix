8208ce27c67c47057df82cec07c7f31542af34d5
ReStranger <strengerplayr@gmail.com>
2024-09-19 03:26:03 +0300

    add: pipewire config

--- hosts/pc/modules/pipewire.nix
+++ hosts/pc/modules/pipewire.nix
@@ -5,5 +5,14 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    extraConfig.pipewire = {
+      "context.properties" = {
+        "default.clock.rate" = 96000;
+        "default.clock.allowed-rates" = [ 44100 48000 88200 96000 ];
+        "default.clock.min-quantum" = 2048;
+        "default.clock.quantum" = 4096;
+        "default.clock.max-quantum" = 8192;
+      };
+    };
   };
 }
