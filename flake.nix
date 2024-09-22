e76654b571d264d3d5c86721e9af4470d7823aba
ReStranger <strengerplayr@gmail.com>
2024-09-22 22:55:13 +0300

    refactor: add home-manager as module

--- flake.nix
+++ flake.nix
@@ -21,22 +21,43 @@
   outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ayugram-desktop, ... }@inputs:
     let
       system = "x86_64-linux";
+      home-manager-module = home-manager.nixosModules.home-manager {
+        home-manager = {
+          useUserPackages = true;
+            users.restranger = import ./home/restranger;
+            extraSpecialArgs = { inherit inputs; };
+          };
+        }; # TODO: add as import
     in
     {
-      nixosConfigurations.pc = nixpkgs.lib.nixosSystem {
-        system = { inherit system; };
-        modules = [ ./hosts/pc ];
-      };
+      nixosConfigurations = {
+        pc = nixpkgs.lib.nixosSystem {
+          system = { inherit system; };
+          modules = [ 
+            ./hosts/pc
+            home-manager.nixosModules.home-manager {
+              home-manager = {
+                useUserPackages = true;
+                users.restranger = import ./home/restranger;
+                extraSpecialArgs = { inherit inputs; };
+              };
+            }
+          ];
+        };
 
-      nixosConfigurations.magicbook = nixpkgs.lib.nixosSystem {
+      magicbook = nixpkgs.lib.nixosSystem {
         system = { inherit system; };
-        modules = [ ./hosts/magicbook ];
-      };
-
-      homeConfigurations.restranger = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs { inherit system; };
-        modules = [ ./home/restranger ];
-        extraSpecialArgs = { inherit inputs; };
+        modules = [ 
+          ./hosts/magicbook 
+          home-manager.nixosModules.home-manager {
+            home-manager = {
+              useUserPackages = true;
+              users.restranger = import ./home/restranger;
+              extraSpecialArgs = { inherit inputs; };
+            };
+          }
+        ];
       };
     };
+  };
 }
