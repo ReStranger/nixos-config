b5d91951e14603050a9d0ff7b40535f54da48edc
ReStranger <strengerplayr@gmail.com>
2024-09-22 22:55:13 +0300

    refactor: add home-manager as module

--- home/restranger/default.nix
+++ home/restranger/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{
   home = {
     username = "restranger";
     homeDirectory = "/home/restranger";
--- home/restranger/modules/desktop-environment/Hyprland/sessionVariables.nix
+++ home/restranger/modules/desktop-environment/Hyprland/sessionVariables.nix
@@ -1,7 +1,7 @@
 { config, osConfig, ... }:
 {
   home.sessionVariables =
-  if ( osConfig != null && osConfig.networking.hostname == "pc") then
+  if ( osConfig != null && osConfig.networking.hostName == "pc") then
     {
       NIXOS_OZONE_WL = "0";
       LIBVA_DRIVER_NAME = "nvidia";
@@ -11,7 +11,7 @@
       __GL_VRR_ALLOWED = 0;
 
       EGL_PLATFORM = "wayland";
-      __EGL_VENDOR_LIBRARY_FILENAMES = "${config.boot.kernelPackages.nvidiaPackages.beta}/share/glvnd/egl_vendor.d/10_nvidia.json";
+      # __EGL_VENDOR_LIBRARY_FILENAMES = "${config.boot.kernelPackages.nvidiaPackages.beta}/share/glvnd/egl_vendor.d/10_nvidia.json";
     }
   else
     {
