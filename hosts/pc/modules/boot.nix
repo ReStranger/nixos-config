33440e4e17acc241fcd6865b719eb735fd3c7ad2
ReStranger <strengerplayr@gmail.com>
2024-09-24 01:59:10 +0300

    add: boot parametrs

--- hosts/pc/modules/boot.nix
+++ hosts/pc/modules/boot.nix
@@ -18,10 +18,26 @@
     };
     kernelModules = [ "kvm-amd" ];
     kernelParams = [
+      "loglevel=3"
       "quiet"
       "splash"
+      "mitigations=off"
+      "nowatchdog"
+      "page_alloc.shuffle=1"
+      "split_lock_detect=off"
+      "pci=pcie_bus_perf"
+      "threadirqs"
+      "noibrs"
+      "tsx_async_abort=off"
+      "rootfstype=btrfs"
+      "selinux=0"
+      "raid=noautodetect"
+      "preempt=none"
+      "hpet=disable"
       "zswap.enabled=0"
       "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+      "nvidia_drm.modeset=1"
+      "nvidia_drm.fbdev=1"
     ];
     extraModulePackages = [
     ];
