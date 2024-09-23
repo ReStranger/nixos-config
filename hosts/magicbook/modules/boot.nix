cafd084372dc32bcb79bc7cbb3129f1d3735844a
ReStranger <strengerplayr@gmail.com>
2024-09-24 01:59:10 +0300

    add: boot parametrs

--- hosts/magicbook/modules/boot.nix
+++ hosts/magicbook/modules/boot.nix
@@ -19,8 +19,22 @@
     };
     kernelModules = [ "kvm-amd" ];
     kernelParams = [
+      "loglevel=3"
       "quiet"
       "splash"
+      "mitigations=off"
+      "nowatchdog"
+      "page_alloc.shuffle=1"
+      "split_lock_detect=off"
+      "pci=pcie_bus_perf"
+      "threadirqs"
+      "noibrs"
+      "tsx_async_abort=off"
+      "rootfstype=btrfs"
+      "selinux=0"
+      "raid=noautodetect"
+      "preempt=none"
+      "hpet=disable"
       "zswap.enabled=0"
       "nmi_watchdog=0"
       "usbcore.autosuspend=5"
