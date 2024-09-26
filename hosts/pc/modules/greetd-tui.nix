e360e83eb8b6c9aebb34f1be5767dc7a6c667db9
ReStranger <strengerplayr@gmail.com>
2024-09-27 02:37:15 +0300

    fix: remove deprecated parametr name

--- hosts/pc/modules/greetd-tui.nix
+++ hosts/pc/modules/greetd-tui.nix
@@ -4,7 +4,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.xserver.displayManager.sessionData.desktops}/share/xsessions:${config.services.xserver.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/xsessions:${config.services.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
         user = "greeter";
       };
     };
