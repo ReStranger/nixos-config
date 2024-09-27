e928ba5ec5d9722c811a9a1d15af531a8615cde6
ReStranger <strengerplayr@gmail.com>
2024-09-28 02:45:58 +0300

    add: qt theme

--- home/restranger/modules/themes/cattpucin-mocha-mauve/qt/default.nix
+++ home/restranger/modules/themes/cattpucin-mocha-mauve/qt/default.nix
@@ -1,17 +1,18 @@
 { pkgs, ...}:
 {
-  environment.systemPackages = with pkgs;
-    [
+    home.packages = with pkgs; [
       libsForQt5.qtstyleplugin-kvantum
       libsForQt5.lightly
     ];
-  environment.variables.QT_QPA_PLATFORMTHEME = "qt5ct";
-  qt = {
-    enable = true;
-    platformTheme = "qt5ct";
-    style = {
-      package = pkgs.catppuccin-kvantum;
-      name = "kvantum";
-    };
+  home.sessionVariables = {
+    QT_QPA_PLATFORMTHEME = "gtk";
   };
+  # qt = {
+  #   enable = true;
+  #   platformTheme = "qt5ct";
+  #   style = {
+  #     package = pkgs.catppuccin-kvantum;
+  #     name = "kvantum";
+  #   };
+  # };
 }
