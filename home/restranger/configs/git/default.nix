b53651f7855f978159d2ca68b4d3d4f0426d67e5
ReStranger <strengerplayr@gmail.com>
2024-09-30 02:57:49 +0300

    add: git settings

--- home/restranger/configs/git/default.nix
+++ home/restranger/configs/git/default.nix
@@ -5,6 +5,11 @@
     userEmail = "strengerplayr@gmail.com";
     extraConfig = {
       init.defaultBranch = "main";
+      http = {
+        postBuffer = 524288000;
+        lowSpeedLimit = 0;
+        lowSpeedTime = 999999;
+      };
     };
   };
 }
