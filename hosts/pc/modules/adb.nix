4ab0c10c801ae0735a55703fba31a71ade8a3b84
ReStranger <strengerplayr@gmail.com>
2024-10-19 23:58:49 +0300

    fix: adb

--- hosts/pc/modules/adb.nix
+++ hosts/pc/modules/adb.nix
@@ -1,17 +1,4 @@
 {
-  lib
-  , config
-  , ...
-}:
-with lib;
-let
-  cfg = config.module.adb;
-in {
-  options = {
-    modules.adb.enable = mkEnableOption "Enable android debug bridge";
-  };
-  config = mkIf cfg.enable {
-    programs.adb.enable = true;
-    users.users.restranger.extraGroups = ["adbusers"];
-  };
+  programs.adb.enable = true;
+  users.users.restranger.extraGroups = ["adbusers"];
 }
