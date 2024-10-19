f241a4143bc3cd06dc4d7f1857e5fc3f4f4c4185
ReStranger <strengerplayr@gmail.com>
2024-10-20 00:27:25 +0300

    add: Initial flake parts support

--- flake.nix
+++ flake.nix
@@ -1,6 +1,54 @@
 {
   description = "ReStranger's nix system configuration";
 
+  outputs = {
+        self
+      , nixpkgs
+      , nixpkgs-stable
+      , flake-parts
+      , home-manager
+      , ayugram-desktop
+      , ...
+    }@inputs:
+    let
+      x86_64 = "x86_64-linux";
+      aarch64 = "aarch64-linux";
+    in flake-parts.lib.mkFlake { inherit inputs; } {
+      flake = {
+        nixosConfigurations = {
+          pc = nixpkgs.lib.nixosSystem {
+            system = { inherit x86_64; };
+            modules = [
+              ./hosts/pc
+              home-manager.nixosModules.home-manager {
+                home-manager = {
+                  backupFileExtension = "backup";
+                  useUserPackages = true;
+                  users.restranger = import ./home/restranger;
+                  extraSpecialArgs = { inherit inputs; };
+                };
+              }
+            ];
+          };
+
+          magicbook = nixpkgs.lib.nixosSystem {
+            system = { inherit x86_64; };
+            modules = [
+              ./hosts/magicbook
+              home-manager.nixosModules.home-manager {
+                home-manager = {
+                  backupFileExtension = "backup";
+                  useUserPackages = true;
+                  users.restranger = import ./home/restranger;
+                  extraSpecialArgs = { inherit inputs; };
+                };
+              }
+            ];
+          };
+      };
+    };
+  };
+
   inputs = {
 
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
@@ -10,50 +58,24 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    nix-topology = {
+      url = "github:oddlama/nix-topology";
+    };
+
+
     ags.url = "github:Aylur/ags";
+
     ayugram-desktop.url = "github:kaeeraa/ayugram-desktop/release?submodules=1";
+
     minimal-tmux = {
       url = "github:niksingh710/minimal-tmux-status";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ayugram-desktop, ... }@inputs:
-    let
-      x86_64 = "x86_64-linux";
-      aarch64 = "aarch64-linux";
-    in
-    {
-      nixosConfigurations = {
-        pc = nixpkgs.lib.nixosSystem {
-          system = { inherit x86_64; };
-          modules = [ 
-            ./hosts/pc
-            home-manager.nixosModules.home-manager {
-              home-manager = {
-                backupFileExtension = "backup";
-                useUserPackages = true;
-                users.restranger = import ./home/restranger;
-                extraSpecialArgs = { inherit inputs; };
-              };
-            }
-          ];
-        };
-
-        magicbook = nixpkgs.lib.nixosSystem {
-          system = { inherit x86_64; };
-          modules = [ 
-            ./hosts/magicbook 
-            home-manager.nixosModules.home-manager {
-              home-manager = {
-                backupFileExtension = "backup";
-                useUserPackages = true;
-                users.restranger = import ./home/restranger;
-                extraSpecialArgs = { inherit inputs; };
-              };
-            }
-          ];
-        };
-      };
-    };
 }
