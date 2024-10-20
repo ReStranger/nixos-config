b72108445a115e43b5e5c7d0c1d22e4a274f76a2
ReStranger <strengerplayr@gmail.com>
2024-10-20 05:15:24 +0300

    add: zapret module

---
+++ system/modules/default.nix
@@ -0,0 +1,10 @@
+{ systemModules
+, lib
+, ...
+}:
+{
+  # Read all directories from systemModules
+  imports = builtins.filter (module: lib.pathIsDirectory module) (
+    map (module: "${systemModules}/${module}") (builtins.attrNames (builtins.readDir systemModules))
+  );
+}
