32b6a9a91fa8dbd8d21ed6e6526455b7d6c0f05e
ReStranger <strengerplayr@gmail.com>
2024-10-20 05:41:45 +0300

    upd: to flake parts

---
+++ hots.nix
@@ -0,0 +1,18 @@
+{
+  hosts = {
+    pc = {
+      username      = "restranger";
+      platform      = "x86_64-linux";
+      stateVersion  = "24.05";
+      isWorkstation = true;
+      de            = "hyprland";
+    };
+    magicbook = {
+      username      = "restranger";
+      platform      = "x86_64-linux";
+      stateVersion  = "24.05";
+      isWorkstation = true;
+      de            = "hyprland";
+    };
+  };
+}
---
+++ modules/nix/default.nix
@@ -0,0 +1,57 @@
+{ inputs
+, lib
+, config
+, pkgs
+, ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.nix-config;
+in {
+  options = {
+    module.nix-config = {
+      enable = mkEnableOption "Enables nix-config";
+
+      useNixPackageManagerConfig = mkOption {
+        type = types.bool;
+        description = "Whether to use custom Nix package manager settings";
+        default = true;
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Nixpkgs config
+    nixpkgs.config = {
+      allowUnfree = true;
+      allowUnfreePerdicate = (_: true);
+    };
+
+    # Nix package manager settings
+    nix = {
+      package = pkgs.nixVersions.latest;
+      registry.s.flake = inputs.self;
+
+      settings = {
+        experimental-features = [ "nix-command" "flakes" ];
+        auto-optimise-store = true;
+
+        substituters = [
+          "https://cache.garnix.io"
+        ];
+
+        trusted-public-keys = [
+          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        ];
+      };
+
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 7d";
+      };
+    };
+  };
+}
