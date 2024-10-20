d72ab2eead8e04d1fd49294b9f697ea3aa4ce61e
ReStranger <strengerplayr@gmail.com>
2024-10-20 05:09:15 +0300

    upd: zapret

--- hosts/pc/modules/services.nix
+++ hosts/pc/modules/services.nix
@@ -15,23 +15,28 @@
     };
     flatpak.enable = true;
     zapret = {
-      enable = false;
+      enable = true;
       params = [
-          "--qnum=0"
-          "--filter-udp=443" 
-          "--dpi-desync=fake"
-          "--dpi-desync-udplen-increment=10"
-          "--dpi-desync-repeats=6"
-          "--dpi-desync-udplen-pattern=0xDEADBEEF"
-          "--new"
-          "--filter-udp=50000-65535" 
-          "--dpi-desync=fake,tamper"
-          "--dpi-desync-any-protocol" 
-          "--new"
-          "--filter-tcp=443" 
-          "--dpi-desync=fake,split2" 
-          "--dpi-desync-autottl=2" 
-          "--dpi-desync-fooling=md5sig" 
+        "--qnum=0"
+        "--dpi-desync=fake"
+        "--dpi-desync-ttl=0"
+        "--dpi-desync-ttl6=0"
+        "--dpi-desync-fooling=badsum"
+        "--hostlist-auto-retrans-threshold=3"
+        "--hostlist-auto-fail-threshold=3"
+        "--hostlist-auto-fail-time=60"
+        "--dpi-desync-repeats=6"
+        "--dpi-desync-any-protocol"
+        "--dpi-desync-udplen-increment=10"
+        "--dpi-desync-udplen-pattern=0xDEADBEEF"
+        "--new"
+        "--filter-udp=50000-65535" 
+        "--dpi-desync=fake,tamper"
+        "--new"
+        "--filter-tcp=443" 
+        "--dpi-desync=fake,split2" 
+        "--dpi-desync-autottl=2" 
+        "--dpi-desync-fooling=md5sig"
         ];
       whitelist = [
           "discord-attachments-uploads-prd.storage.googleapis.com"
