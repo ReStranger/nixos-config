a8a2ee87db37ac0b2b7ef7d72f9dbea739dfbe1d
ReStranger <strengerplayr@gmail.com>
2024-10-21 14:43:04 +0300

    add: zram module

---
+++ system/modules/services/zram/default.nix
@@ -0,0 +1,33 @@
+{
+  lib
+  , config
+  , ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.services.zram;
+in {
+  options.modules.zram = {
+    enable = mkEnableOption "Enable zram";
+      deviceNumber = mkOption {
+        type = types.number;
+        default = 1;
+        description = ''
+          Change number of ram devices
+        '';
+    };
+  };
+  config = mkIf cfg.enable {
+    zramSwap = {
+      enable = true;
+      priority = 32767;
+      swapDevices = cfg.deviceNumber;
+      memoryPercent = 100;
+    };
+    boot.kernelParams = [
+      "zswap.enabled=0"
+    ];
+  };
+}
