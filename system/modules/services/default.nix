fe0a7948833acea8646c65a131d98574c8e9cf9f
ReStranger <strengerplayr@gmail.com>
2024-10-21 14:43:20 +0300

    add: services setup

---
+++ system/modules/services/default.nix
@@ -0,0 +1,12 @@
+{ systemModules
+, lib
+, ...
+}:
+
+let
+  hostServicesModulesPath = "${systemModules}/services";
+in {
+  imports = builtins.filter (module: lib.pathIsDirectory module) (
+    map (module: "${hostServicesModulesPath}/${module}") (builtins.attrNames (builtins.readDir hostServicesModulesPath))
+  );
+}
