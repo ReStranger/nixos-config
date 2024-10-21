a7349eabe5c08ac84ccaf8a752c2df5a80b7cbdd
ReStranger <strengerplayr@gmail.com>
2024-10-21 14:32:27 +0300

    add: timezone module

---
+++ system/modules/timezone/default.nix
@@ -0,0 +1,16 @@
+{ lib
+  , config
+  , ...
+}:
+with lib;
+
+let
+  cfg = config.module.timezone;
+in {
+  options.module.timezone = {
+    enable = mkEnableOption "Set timezone to Moscow";
+  };
+  config = mkIf cfg.enable {
+    time.timeZone = "Europe/Moscow";
+  };
+}
