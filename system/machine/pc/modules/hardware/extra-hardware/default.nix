0a3b3bfa696f201e09a14d4b3e1b8303c8c8790a
ReStranger <strengerplayr@gmail.com>
2024-10-22 22:51:45 +0300

    pc refactor

---
+++ system/machine/pc/modules/hardware/extra-hardware/default.nix
@@ -0,0 +1,19 @@
+{ lib
+, pkgs
+, config
+, ...
+}:
+{
+  # Extra drivers settings
+  hardware = {
+    enableAllFirmware = true;
+    enableRedistributableFirmware = true;
+    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+    firmware = with pkgs; [
+      linux-firmware
+    ];
+
+  };
+  powerManagement.cpuFreqGovernor = "performance";
+}
---
+++ system/machine/pc/modules/hardware/graphics-card/default.nix
@@ -0,0 +1,26 @@
+{ config
+  , pkgs
+  , ...
+}:
+{
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      nvidia-vaapi-driver
+      cudaPackages.cudatoolkit
+    ];
+  };
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+
+    modesetting.enable = true;
+    powerManagement.enable = true;
+
+    powerManagement.finegrained = false;
+
+    open = false;
+    nvidiaSettings = false;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
