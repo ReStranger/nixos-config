016c9be62b2d14e5444ff6af3e61bc8a671f9fa3
ReStranger <strengerplayr@gmail.com>
2024-10-23 01:00:15 +0300

    add: upower service

---
+++ system/modules/services/upower/default.nix
@@ -0,0 +1,16 @@
+{
+  lib
+  , config
+  , ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.services.upower;
+in {
+  options.module.services.upower.enable = mkEnableOption "Enable upower";
+  config = mkIf cfg.enable {
+    services.upower.enable = true;
+  };
+}
