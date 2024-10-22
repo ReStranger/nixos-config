5548ed0e807cc7afa0b4b3301d267ff99c91e128
ReStranger <strengerplayr@gmail.com>
2024-10-22 23:25:38 +0300

    add: tty module

---
+++ system/modules/tty/default.nix
@@ -0,0 +1,19 @@
+{ lib
+  , config
+  , pkgs
+  , ...
+}:
+with lib;
+let
+  cfg = config.services.zapret;
+in {
+  options.modules.tty.enable = mkEnableOption "Enable TTY setup module";
+  config = mkIf cfg.enable {
+    console = {
+      earlySetup = true;
+      font = "ter-v32n";
+      packages = with pkgs; [ terminus_font ];
+      keyMap = "us";
+    };
+  };
+}
