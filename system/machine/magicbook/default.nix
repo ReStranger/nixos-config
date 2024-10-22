8915d011c382bf5e8cbfa390bf12c5fabe3f5c15
ReStranger <strengerplayr@gmail.com>
2024-10-23 01:00:15 +0300

    add: upower service

--- system/machine/magicbook/default.nix
+++ system/machine/magicbook/default.nix
@@ -23,6 +23,7 @@
       polkit.enable = true;
       power-profiles-daemon.enable = true;
       systemd-oomd.enable = true;
+      upower.enable = true;
       zapret-config.enable = true;
       zerotier-one.enable = true;
       zram = {
