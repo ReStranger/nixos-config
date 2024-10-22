ac23eda45b415bba77c8353046d9f4da796236f8
ReStranger <strengerplayr@gmail.com>
2024-10-23 00:23:35 +0300

    add: auto-cpufreq module

---
+++ system/modules/services/auto-cpufreq/default.nix
@@ -0,0 +1,26 @@
+{
+  lib
+  , config
+  , ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.services.auto-cpufreq;
+in {
+  options.module.services.auto-cpufreq.enable = mkEnableOption "Enable auto-cpufreq";
+  config = mkIf cfg.enable {
+    services.auto-cpufreq.enable = true;
+    services.auto-cpufreq.settings = {
+      battery = {
+        governor = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
+    };
+  };
+}
