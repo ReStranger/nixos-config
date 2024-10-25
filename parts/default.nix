41c1083011590fada8a78b3f8667ddb0fb579b73
ReStranger <strengerplayr@gmail.com>
2024-10-25 11:33:33 +0300

    add: treefmt

---
+++ parts/default.nix
@@ -0,0 +1,9 @@
+{ lib
+, ...
+}:
+
+{
+  imports = builtins.filter (module: lib.pathIsDirectory module) (
+    map (module: builtins.toString ./. + "/${module}") (builtins.attrNames (builtins.readDir (builtins.toString ./.)))
+  );
+}
