a0a88af179c3c85d2ad48778b9e1fff4170d5960
ReStranger <strengerplayr@gmail.com>
2024-10-25 11:33:33 +0300

    add: treefmt

---
+++ parts/treefmt/default.nix
@@ -0,0 +1,21 @@
+{ inputs
+, ...
+}:
+
+{
+  imports = [
+    inputs.treefmt-nix.flakeModule
+  ];
+
+  perSystem = { config, ... }: {
+    # For nix fmt
+    treefmt.config = {
+      projectRootFile = "flake.nix";
+
+      programs = {
+        deadnix.enable = true;
+        statix.enable = true;
+      };
+    };
+  };
+}
