2a2684b351f0708abf445238e01ae6e505d1ea26
ReStranger <strengerplayr@gmail.com>
2024-10-30 17:53:29 +0300

    add: hyprland git

--- system/modules/programs/hyprland/default.nix
+++ system/modules/programs/hyprland/default.nix
@@ -14,8 +14,8 @@ in {
   config = mkIf cfg.enable {
     programs.hyprland = {
       enable = true;
-      # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-      # portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+      portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
       xwayland.enable = true;
     };
     services.libinput.enable = true;
