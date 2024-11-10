e5d7c3d9aa91f313f211cc455ae32ba98506e0c3
ReStranger <strengerplayr@gmail.com>
2024-11-10 18:43:56 +0300

    add: fontDir to font module

--- system/modules/programs/fonts/default.nix
+++ system/modules/programs/fonts/default.nix
@@ -12,14 +12,17 @@ in {
   options.module.programs.fonts.enable = mkEnableOption "Enable System fonts";
 
   config = mkIf cfg.enable {
-    fonts.packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-emoji
-      liberation_ttf
-      corefonts
-      vistafonts
-      (nerdfonts.override { fonts = [ "JetBrainsMono" "CascadiaCode" ]; })
-    ];
+    fonts = {
+      fontDir.enable = true;
+      packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-emoji
+        liberation_ttf
+        corefonts
+        vistafonts
+        (nerdfonts.override { fonts = [ "JetBrainsMono" "CascadiaCode" ]; })
+      ];
+    };
   };
 }
