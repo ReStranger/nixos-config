768bbaa1322ceac68b9003b8a773171bdecef5a1
ReStranger <strengerplayr@gmail.com>
2024-11-10 22:58:53 +0300

    system/modules: Add bluetooth modules
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- templates/module/services/default.nix
+++ templates/module/services/default.nix
@@ -1,1 +1,17 @@
+{ config
+, lib
+, ...
+}:
 
+with lib;
+
+let
+  cfg = config.module.service.name;
+in
+{
+  options.module.service.name = {
+    enable = mkEnableOption "Enable name program";
+  };
+
+  config = mkIf cfg.enable { };
+}
