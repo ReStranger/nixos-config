333ca0405ca6f2a28f553b709457538fa2d1f4b8
ReStranger <strengerplayr@gmail.com>
2024-11-10 05:06:17 +0300

    add: btop module

---
+++ home/modules/btop/default.nix
@@ -0,0 +1,28 @@
+{ config
+, lib
+, ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.btop;
+in {
+  options.module.btop = {
+    enable = mkEnableOption "Enable btop module";
+  };
+
+  config = mkIf cfg.enable {
+    programs.btop = {
+      enable = true;
+      settings = {
+        theme_background = false;
+        truecolor = true;
+        force_tty = false;
+        vim_keys = true;
+        rounded_corners = true;
+        update_ms = 300;
+      };
+    };
+  };
+}
