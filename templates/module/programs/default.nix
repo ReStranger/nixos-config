d5219a72271f8129c265c2d8368c01c1fcb2dcc7
ReStranger <strengerplayr@gmail.com>
2024-11-10 04:56:19 +0300

    add: programs template

---
+++ templates/module/programs/default.nix
@@ -0,0 +1,17 @@
+{ config
+, lib
+, ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.programs.name;
+in {
+  options.module.programs.name = {
+    enable = mkEnableOption "Enable name program";
+  };
+
+  config = mkIf cfg.enable {
+  };
+}
