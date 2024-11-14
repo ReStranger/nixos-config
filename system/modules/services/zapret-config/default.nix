6fb05df3bc4753e0b94285bce1a2129556c694ed
ReStranger <strengerplayr@gmail.com>
2024-11-15 02:35:01 +0300

    fix: zapret

--- system/modules/services/zapret-config/default.nix
+++ system/modules/services/zapret-config/default.nix
@@ -14,42 +14,56 @@ in
     settings = mkOption {
       type = types.lines;
       default = ''
+        FWTYPE=nftables
         SET_MAXELEM=522288
         IPSET_OPT="hashsize 262144 maxelem $SET_MAXELEM"
-
         IP2NET_OPT4="--prefix-length=22-30 --v4-threshold=3/4"
         IP2NET_OPT6="--prefix-length=56-64 --v6-threshold=5"
         AUTOHOSTLIST_RETRANS_THRESHOLD=3
         AUTOHOSTLIST_FAIL_THRESHOLD=3
         AUTOHOSTLIST_FAIL_TIME=60
         AUTOHOSTLIST_DEBUGLOG=0
-
         MDIG_THREADS=30
 
         GZIP_LISTS=1
-        QUIC_PORTS=50000-65535
-
-        MODE_HTTP=1
-        MODE_HTTP_KEEPALIVE=0
-        MODE_HTTPS=1
-        MODE_QUIC=1
-        MODE_FILTER=none
-
         DESYNC_MARK=0x40000000
         DESYNC_MARK_POSTNAT=0x20000000
-        NFQWS_OPT_DESYNC="--dpi-desync=fake --dpi-desync-ttl=0 --dpi-desync-ttl6=0 --dpi-desync-fooling=badsum"
-        NFQWS_OPT_DESYNC_HTTP="--dpi-desync=split --dpi-desync-ttl=5"
-        NFQWS_OPT_DESYNC_HTTPS="--dpi-desync=fake --dpi-desync-ttl=5"
-        NFQWS_OPT_DESYNC_QUIC="--dpi-desync=fake,tamper --dpi-desync-repeats=15 --dpi-desync-any-protocol"
-        TPWS_OPT="--hostspell=HOST --split-http-req=method --split-pos=3 --hostcase --oob"
+
+        TPWS_SOCKS_ENABLE=0
+        TPPORT_SOCKS=987
+        TPWS_SOCKS_OPT="
+        --filter-tcp=80 --methodeol <HOSTLIST> --new
+        --filter-tcp=443 --split-pos=1,midsld --disorder <HOSTLIST>
+        "
+
+        TPWS_ENABLE=0
+        TPWS_PORTS=80,443
+        TPWS_OPT="
+        --filter-tcp=80 --methodeol <HOSTLIST> --new
+        --filter-tcp=443 --split-pos=1,midsld --disorder <HOSTLIST>
+        "
+
+        NFQWS_ENABLE=1
+        NFQWS_PORTS_TCP="$(echo $NFQWS_OPT | grep -oE 'filter-tcp=[0-9,-]+' | sed -e 's/.*=//g' -e 's/,/\n/g' | sort -un)";
+        NFQWS_PORTS_UDP="$(echo $NFQWS_OPT | grep -oE 'filter-udp=[0-9,-]+' | sed -e 's/.*=//g' -e 's/,/\n/g' | sort -un)";
+        NFQWS_TCP_PKT_OUT=$((6+$AUTOHOSTLIST_RETRANS_THRESHOLD))
+        NFQWS_TCP_PKT_IN=3
+        NFQWS_UDP_PKT_OUT=$((6+$AUTOHOSTLIST_RETRANS_THRESHOLD))
+        NFQWS_UDP_PKT_IN=0
+        NFQWS_OPT="
+        --filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-fooling=md5sig <HOSTLIST_NOAUTO> --new
+        --filter-tcp=443 --dpi-desync=fake,disorder2 --dpi-desync-fooling=md5sig <HOSTLIST_NOAUTO> --new
+        --filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=6 <HOSTLIST_NOAUTO> --new
+        --filter-udp=50000-50099 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-fake-quic=0xC3
+        "
+
+        MODE_FILTER=autohostlist
 
         FLOWOFFLOAD=donttouch
 
         INIT_APPLY_FW=1
 
         DISABLE_IPV6=1
-
-        GETLIST=get_antifilter_ipsmart.sh
       '';
       example = ''
         TPWS_OPT="--hostspell=HOST --split-http-req=method --split-pos=3 --oob"
@@ -65,7 +79,7 @@ in
         "iptables"
         "nftables"
       ];
-      default = "iptables";
+      default = "nftables";
       description = ''
         Which firewall zapret should use
       '';
@@ -78,21 +92,9 @@ in
         Disable or enable usage of IpV6 by zapret
       '';
     };
-    mode = mkOption {
-      type = types.enum [
-        "tpws"
-        "tpws-socks"
-        "nfqws"
-        "filter"
-        "custom"
-      ];
-      default = "nfqws";
-      description = ''
-        Which mode zapret should use
-      '';
-    };
   };
   config = mkIf cfg.enable {
+    boot.kernel.sysctl = { "net.netfilter.nf_conntrack_tcp_be_liberal" = 1; };
     users.users.tpws = {
       isSystemUser = true;
       group = "tpws";
@@ -106,6 +108,7 @@ in
         (if cfg.firewallType == "iptables" then iptables else nftables)
         gawk
         ipset
+        sysctl
       ];
       serviceConfig = {
         Type = "forking";
@@ -119,7 +122,6 @@ in
         ExecStop = "${cfg.package}/bin/zapret stop";
         EnvironmentFile = pkgs.writeText "${cfg.package.pname}-environment" (concatStrings [
           ''
-            MODE=${cfg.mode}
             FWTYPE=${cfg.firewallType}
             DISABLE_IPV6=${if cfg.disableIpv6 then "1" else "0"}
           ''
