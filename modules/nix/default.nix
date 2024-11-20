2752901313300235244d42d95becba950a53099c
ReStranger <strengerplayr@gmail.com>
2024-11-21 02:21:28 +0300

    fix keys :D

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -46,8 +46,8 @@ in
         ];
 
         trusted-public-keys = [
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
         ];
       };
