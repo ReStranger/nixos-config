a5eef6a604c3b86a992f077d35e9d7a4ee3fa5ed
ReStranger <strengerplayr@gmail.com>
2024-11-25 20:22:05 +0300

    system/modules/services/opentablet: Add opentablet driver module
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

---
+++ system/modules/services/opentablet/default.nix
@@ -0,0 +1,21 @@
+{ lib
+, config
+, ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.services.opentablet;
+in
+{
+  options.module.services.opentablet.enable = mkEnableOption "Enable opentablet";
+  config = mkIf cfg.enable {
+    hardware.opentabletdriver = {
+      enable = true;
+      daemon.enable = true;
+    };
+  };
+}
+
+
