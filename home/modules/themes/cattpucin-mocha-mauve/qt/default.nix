e53a61f67e3cb0cd9f399713674c0c157da9caff
ReStranger <strengerplayr@gmail.com>
2024-12-04 19:55:26 +0300

    home: Add qt catppuccin theme
    - Add overlay with new versions of catppuccin-qt5ct
    - Add qt5ct settings
    
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- home/modules/themes/cattpucin-mocha-mauve/qt/default.nix
+++ home/modules/themes/cattpucin-mocha-mauve/qt/default.nix
@@ -1,18 +1,60 @@
-{ pkgs, ... }:
+{ config
+, lib
+, pkgs
+, ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.theme.cattpucin-mocha-mauve.qt;
+in
 {
-  home.packages = with pkgs; [
-    libsForQt5.qtstyleplugin-kvantum
-    libsForQt5.lightly
-  ];
-  home.sessionVariables = {
-    QT_QPA_PLATFORMTHEME = "gtk";
+  options.module.theme.cattpucin-mocha-mauve.qt = {
+    enable = mkEnableOption "Enable qt cattpucin-mocha-mauve theme";
   };
-  # qt = {
-  #   enable = true;
-  #   platformTheme = "qt5ct";
-  #   style = {
-  #     package = pkgs.catppuccin-kvantum;
-  #     name = "kvantum";
-  #   };
-  # };
+
+  config = mkIf cfg.enable {
+    qt = {
+      enable = true;
+      platformTheme.name = "qt5ct";
+      style = {
+        package = pkgs.catppuccin-qt5ct;
+      };
+    };
+    home.file.".config/qt5ct/qt5ct.conf".text = /*conf*/ ''
+[Appearance]
+color_scheme_path=/home/restranger/.nix-profile/share/qt5ct/colors/catppuccin-mocha-mauve.conf
+custom_palette=true
+icon_theme=Tela-circle-dracula-dark
+standard_dialogs=gtk3
+style=Fusion
+
+[Fonts]
+fixed="Gabarito,10,-1,5,50,0,0,0,0,0,Regular"
+general="Gabarito,10,-1,5,50,0,0,0,0,0,Regular"
+
+[Interface]
+activate_item_on_single_click=1
+buttonbox_layout=0
+cursor_flash_time=1000
+dialog_buttons_have_icons=1
+double_click_interval=400
+gui_effects=@Invalid()
+keyboard_scheme=2
+menus_have_icons=true
+show_shortcuts_in_context_menus=true
+stylesheets=@Invalid()
+toolbutton_style=4
+underline_shortcut=1
+wheel_scroll_lines=3
+
+[SettingsWindow]
+geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\0\0\0\0\0\0\0\0\x2\x1e\0\0\x2L\0\0\0\0\0\0\0\0\0\0\x2\x1e\0\0\x2L\0\0\0\0\x2\0\0\0\a\x80\0\0\0\0\0\0\0\0\0\0\x2\x1e\0\0\x2L)
+
+[Troubleshooting]
+force_raster_widgets=1
+ignored_applications=@Invalid()
+'';
+  };
 }
