597f8378dc03c6dd351bf964fcbb9887053c4adb
ReStranger <strengerplayr@gmail.com>
2024-12-07 03:45:01 +0300

    system/modules/programs/systemPackages: Disable nano
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- system/modules/programs/systemPackages/default.nix
+++ system/modules/programs/systemPackages/default.nix
@@ -13,6 +13,7 @@ in
   options.module.programs.systemPackages.enable = mkEnableOption "Enable System Software";
 
   config = mkIf cfg.enable {
+    programs.nano.enable = false;
     environment.systemPackages = with pkgs; [
       # Utils
       neovim
