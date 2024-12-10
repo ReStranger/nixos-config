08f2476b2eedcde9eae1089a6fddaa2ea091ca7a
ReStranger <strengerplayr@gmail.com>
2024-12-11 02:47:03 +0300

    home/modules/hyprland: Refactor hyprland modules

--- home/modules/hyprland/monitors/default.nix
+++ home/modules/hyprland/monitors/default.nix
@@ -1,48 +1,60 @@
-{ config
-, lib
-, hostname
-, ...
-}:
+{ config, lib, hostname, ... }:
 
 with lib;
 
 let
   cfg = config.module.hyprland.monitors;
 
-  monitorsSettings =
-    if hostname == "pc" then [ "HDMI-A-1, 1920x1080@75, 0x0, 1" "DVI-D-1,disable" ]
-    else if hostname == "magicbook" then [ "eDP-1, 1920x1080@60, 0x0, 1.25" ]
-    else [ ", preferred, auto, 1" ];
+  commonMonitorSettings = [ ", preferred, auto, 1" ];
 
-  workspacesSettings =
-    if hostname == "pc" then
-      [
-        "1,monitor:HDMI-A-1,default:true"
-        "2,monitor:HDMI-A-1"
-        "3,monitor:HDMI-A-1"
-        "4,monitor:HDMI-A-1"
-        "5,monitor:HDMI-A-1"
-        "6,monitor:HDMI-A-1"
-        "7,monitor:DVI-D-1,default:true"
-        "8,monitor:DVI-D-1"
-        "9,monitor:DVI-D-1"
-        "0,monitor:DVI-D-1"
-      ]
-    else if hostname == "magicbook" then
-      [
-        "1,monitor:eDP-1,default:true"
-        "2,monitor:eDP-1"
-        "3,monitor:eDP-1"
-        "4,monitor:eDP-1"
-        "5,monitor:eDP-1"
-        "6,monitor:eDP-1"
-        "7,monitor:eDP-1"
-      ]
-    else [ ];
+  hostSpecificMonitorSettings = if hostname == "pc" then
+    [
+      "HDMI-A-1, 1920x1080@75, 0x0, 1"
+      "DVI-D-1,disable"
+    ]
+  else if hostname == "magicbook" then
+    [
+      "eDP-1, 1920x1080@60, 0x0, 1.25"
+    ]
+  else
+    [ ];
+
+  monitorsSettings = commonMonitorSettings ++ hostSpecificMonitorSettings;
+
+  commonWorkspacesSettings = [ ];
+
+  hostSpecificWorkspacesSettings = if hostname == "pc" then
+    [
+      "1,monitor:HDMI-A-1,default:true"
+      "2,monitor:HDMI-A-1"
+      "3,monitor:HDMI-A-1"
+      "4,monitor:HDMI-A-1"
+      "5,monitor:HDMI-A-1"
+      "6,monitor:HDMI-A-1"
+      "7,monitor:DVI-D-1,default:true"
+      "8,monitor:DVI-D-1"
+      "9,monitor:DVI-D-1"
+      "0,monitor:DVI-D-1"
+    ]
+  else if hostname == "magicbook" then
+    [
+      "1,monitor:eDP-1,default:true"
+      "2,monitor:eDP-1"
+      "3,monitor:eDP-1"
+      "4,monitor:eDP-1"
+      "5,monitor:eDP-1"
+      "6,monitor:eDP-1"
+      "7,monitor:eDP-1"
+    ]
+  else
+    [ ];
+
+  workspacesSettings = commonWorkspacesSettings ++ hostSpecificWorkspacesSettings;
+
 in
 {
   options = {
-    module.hyprland.monitors.enable = mkEnableOption "Enables monitors in Hyprland";
+    module.hyprland.monitors.enable = mkEnableOption "Enable monitors in Hyprland";
   };
 
   config = mkIf cfg.enable {
--- home/modules/hyprland/variables/default.nix
+++ home/modules/hyprland/variables/default.nix
@@ -1,81 +1,59 @@
-{ config
-, lib
-, hostname
-, ...
+{ 
+    config
+    , lib
+    , hostname
+    , ... 
 }:
-
-# TODO: do this more elegantly
 
 with lib;
 
 let
   cfg = config.module.hyprland.variables;
 
-  sessionVariables =
-    if hostname == "pc" then
-      {
-        XDG_CURRENT_DESKTOP = "Hyprland";
-        XDG_SESSION_TYPE = "wayland";
-        XDG_SESSION_DESKTOP = "Hyprland";
+  commonSessionVariables = {
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "Hyprland";
 
-        _JAVA_AWT_WM_NONREPARENTING = 1;
-        GDK_BACKEND = "wayland,x11";
-        SDL_VIDEODRIVER = "wayland,x11,windows";
-        CLUTTER_BACKEND = "wayland";
-        ELECTRON_OZONE_PLATFORM_HINT = "auto";
-        DESKTOPINTEGRATION = "1";
-        MANGOHUD = "1";
+    _JAVA_AWT_WM_NONREPARENTING = 1;
+    GDK_BACKEND = "wayland,x11";
+    SDL_VIDEODRIVER = "wayland,x11,windows";
+    CLUTTER_BACKEND = "wayland";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+    DESKTOPINTEGRATION = "1";
+    MANGOHUD = "1";
 
-        MOZ_ENABLE_WAYLAND = "1";
-        MOZ_DISABLE_RDD_SANDBOX = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    MOZ_DISABLE_RDD_SANDBOX = "1";
 
-        QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-        QT_QPA_PLATFORM = "wayland;xcb ";
-        QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_QPA_PLATFORM = "wayland;xcb ";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
 
-        XCURSOR_SIZE = "24";
-        HYPRCURSOR_SIZE = "24";
+    XCURSOR_SIZE = "24";
+    HYPRCURSOR_SIZE = "24";
+  };
 
-        NIXOS_OZONE_WL = "0";
-        LIBVA_DRIVER_NAME = "nvidia";
-        GBM_BACKEND = "nvidia-drm";
-        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-        NVD_BACKEND = "direct";
-        __GL_VRR_ALLOWED = 0;
+  hostSpecificVariables = if hostname == "pc" then {
+    NIXOS_OZONE_WL = "0";
+    LIBVA_DRIVER_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    NVD_BACKEND = "direct";
+    __GL_VRR_ALLOWED = 0;
 
-        EGL_PLATFORM = "wayland";
-        # __EGL_VENDOR_LIBRARY_FILENAMES = "${config.boot.kernelPackages.nvidiaPackages.stable}/share/glvnd/egl_vendor.d/10_nvidia.json";
-      }
-    else
-      {
-        XDG_CURRENT_DESKTOP = "Hyprland";
-        XDG_SESSION_TYPE = "wayland";
-        XDG_SESSION_DESKTOP = "Hyprland";
+    EGL_PLATFORM = "wayland";
+    # __EGL_VENDOR_LIBRARY_FILENAMES = "${config.boot.kernelPackages.nvidiaPackages.stable}/share/glvnd/egl_vendor.d/10_nvidia.json";
+  } else {
+    NIXOS_OZONE_WL = "1";
+  };
 
-        _JAVA_AWT_WM_NONREPARENTING = 1;
-        GDK_BACKEND = "wayland,x11";
-        SDL_VIDEODRIVER = "wayland,x11,windows";
-        CLUTTER_BACKEND = "wayland";
-        ELECTRON_OZONE_PLATFORM_HINT = "auto";
-        DESKTOPINTEGRATION = "1";
-        MANGOHUD = "1";
+  sessionVariables = commonSessionVariables // hostSpecificVariables;
 
-        MOZ_ENABLE_WAYLAND = "1";
-        MOZ_DISABLE_RDD_SANDBOX = "1";
-
-        QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-        QT_QPA_PLATFORM = "wayland;xcb ";
-        QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-
-        XCURSOR_SIZE = "24";
-        HYPRCURSOR_SIZE = "24";
-
-        NIXOS_OZONE_WL = "1";
-      };
 in
 {
   options = {
-    module.hyprland.variables.enable = mkEnableOption "Enables variables in Hyprland";
+    module.hyprland.variables.enable = mkEnableOption "Enable variables in Hyprland";
   };
 
   config = mkIf cfg.enable {
