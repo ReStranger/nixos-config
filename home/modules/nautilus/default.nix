542b7f283625d627f5f8d09254c8576f30c1787d
ReStranger <strengerplayr@gmail.com>
2024-12-16 23:04:57 +0300

    home/modules/nautilus: Add nautilus module
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

---
+++ home/modules/nautilus/default.nix
@@ -0,0 +1,35 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.nautilus;
+in
+{
+  options.module.nautilus = {
+    enable = mkEnableOption "Enable nautilus module";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+        nautilus
+        nautilus-python
+        nautilus-open-any-terminal
+    ];
+    home.sessionVariables = {
+        NAUTILUS_4_EXTENSION_DIR = "${pkgs.nautilus-python}/lib/nautilus/extensions-4";
+    };
+        
+    
+    dconf = {
+      settings."org/gnome/nautilus/icon-view" = {
+        default-zoom-level = "small-plus";
+      };
+    };
+  };
+}
+
