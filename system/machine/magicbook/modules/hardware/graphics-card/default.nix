3ddba5530e8d9c48521d969405cd7087b5ed5111
ReStranger <strengerplayr@gmail.com>
2024-12-20 15:56:28 +0300

    system/machine/magicbook/modules/hardware/graphics-card: Improve setup
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- system/machine/magicbook/modules/hardware/graphics-card/default.nix
+++ system/machine/magicbook/modules/hardware/graphics-card/default.nix
@@ -1,8 +1,17 @@
-{ ...
+{ pkgs
+, ...
 }:
 {
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
   };
   services.xserver.videoDrivers = [ "amdgpu" ];
+  hardware.opengl.extraPackages = with pkgs; [
+    rocmPackages.clr.icd
+    amdvlk
+  ];
+  hardware.opengl.extraPackages32 = with pkgs; [
+    driversi686Linux.amdvlk
+  ];
 }
