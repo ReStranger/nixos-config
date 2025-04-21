4ca5808db897afae2bc2acdf581cb70a0db54534
ReStranger <restranger@disroot.org>
2025-04-22 01:08:45 +0300

    feat(zerotier-one): Drop sshd_network option
    Signed-off-by: ReStranger <restranger@disroot.org>

--- system/nixos/modules/services/zerotier-one/default.nix
+++ system/nixos/modules/services/zerotier-one/default.nix
@@ -10,22 +10,8 @@ let
   cfg = config.module.services.zerotier-one;
 in
 {
-  options.module.services.zerotier-one = {
-    enable = mkEnableOption "Enable zerotier-one";
-    sshd_network = mkOption {
-      type = types.bool;
-      default = true;
-      description = ''
-        Join to my sshd zerotier network
-      '';
-    };
-  };
+  options.module.services.zerotier-one.enable = mkEnableOption "Enable zerotier-one";
   config = mkIf cfg.enable {
-    services.zerotierone = {
-      enable = true;
-      joinNetworks = mkIf cfg.sshd_network [
-        "e3918db48305d83a"
-      ];
-    };
+    services.zerotierone.enable = true;
   };
 }
