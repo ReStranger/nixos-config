6ef3469895358f1061bbdaf53a23ebecb38028a7
ReStranger <restranger@disroot.org>
2025-05-04 02:06:38 +0300

    feat(wezterm): Set window_background_opacity to 0.90
    Signed-off-by: ReStranger <restranger@disroot.org>

--- home/modules/wezterm/default.nix
+++ home/modules/wezterm/default.nix
@@ -34,7 +34,7 @@ in
           config.enable_tab_bar = true
           config.hide_tab_bar_if_only_one_tab = true
 
-          config.window_background_opacity = 0.87
+          config.window_background_opacity = 0.90
 
           config.window_padding = {
           	left = 0,
