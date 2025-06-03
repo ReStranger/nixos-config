dd2c2ae16368b4ecb1718b9f8b1cad607821188b
ReStranger <restranger@disroot.org>
2025-06-03 21:36:46 +0300

    feat(zram): Improve zram settings
    Signed-off-by: ReStranger <restranger@disroot.org>

--- system/nixos/modules/services/zram/default.nix
+++ system/nixos/modules/services/zram/default.nix
@@ -30,8 +30,16 @@ in
       priority = 32767;
       swapDevices = cfg.deviceNumber;
     };
-    boot.kernelParams = [
-      "zswap.enabled=0"
-    ];
+    boot = {
+      kernelParams = [
+        "zswap.enabled=0"
+      ];
+      kernel.sysctl = {
+        "vm.swappiness" = 180;
+        "vm.watermark_boost_factor" = 0;
+        "vm.watermark_scale_factor" = 125;
+        "vm.page-cluster" = 0;
+      };
+    };
   };
 }
