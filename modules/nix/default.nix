eb1f88118fea7a9988f8a12edc8662b9f1689970
ReStranger <restranger@disroot.org>
2025-06-12 18:04:53 +0300

    chore(hyprland): Drop flake
    * Not needed, because haven't bin cache on main branch
    
    Signed-off-by: ReStranger <restranger@disroot.org>

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -59,7 +59,6 @@ in
 
         substituters = [
           "https://cache.garnix.io"
-          "https://hyprland.cachix.org"
           "https://nyx.chaotic.cx"
           "https://wezterm.cachix.org"
           "https://anyrun.cachix.org"
@@ -67,8 +66,6 @@ in
 
         trusted-public-keys = [
           "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-
           "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
           "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
           "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
