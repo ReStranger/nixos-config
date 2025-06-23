20ff277c6162d31b5e0827975ec437c970295904
ReStranger <restranger@disroot.org>
2025-06-24 02:26:20 +0300

    feat(wezterm): Set bg opacity to 0.98
    Signed-off-by: ReStranger <restranger@disroot.org>

--- home/modules/wezterm/default.nix
+++ home/modules/wezterm/default.nix
@@ -34,7 +34,7 @@ in
           config.enable_tab_bar = true
           config.hide_tab_bar_if_only_one_tab = true
 
-          config.window_background_opacity = 1.0
+          config.window_background_opacity = 0.98
 
           config.window_padding = {
           	left = 0,
