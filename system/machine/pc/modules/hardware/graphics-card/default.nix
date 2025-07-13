07e695d59fef925d8bdb7de02c4c3bb546459821
ReStranger <restranger@disroot.org>
2025-07-13 14:48:05 +0300

    fix(pc/graphics-card): OpenCL setup
    Signed-off-by: ReStranger <restranger@disroot.org>

--- system/machine/pc/modules/hardware/graphics-card/default.nix
+++ system/machine/pc/modules/hardware/graphics-card/default.nix
@@ -3,16 +3,30 @@
   ...
 }:
 {
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [
-      rocmPackages.clr.icd
-      amdvlk
-    ];
-    extraPackages32 = with pkgs; [
-      driversi686Linux.amdvlk
-    ];
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        amdvlk
+        mesa
+        mesa.opencl
+      ];
+      extraPackages32 = with pkgs; [
+        driversi686Linux.amdvlk
+      ];
+    };
+    amdgpu = {
+      initrd.enable = true;
+      opencl.enable = true;
+      amdvlk = {
+        enable = true;
+        support32Bit.enable = true;
+      };
+    };
+  };
+  environment.variables = {
+    RUSTICL_ENABLE = "radeonsi";
   };
   services.xserver.videoDrivers = [ "amdgpu" ];
 }
