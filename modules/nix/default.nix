3970f04d32b211d5d24394da0c79476f76cf5a1d
ReStranger <restranger@disroot.org>
2025-10-03 21:00:37 +0300

    feat(nix): Disable automatic garbage collection
    Signed-off-by: ReStranger <restranger@disroot.org>

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -72,10 +72,6 @@ in
           "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
         ];
       };
-      gc = {
-        automatic = true;
-        options = "--delete-older-than 7d";
-      };
     };
   };
 }
