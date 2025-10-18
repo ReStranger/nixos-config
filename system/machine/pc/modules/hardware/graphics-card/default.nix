e5d1e1e7eac795bebb027d34f5194e2c519975c1
ReStranger <restranger@disroot.org>
2025-10-18 14:22:45 +0300

    chore(grapthics-card): Drop amdvlk
    Signed-off-by: ReStranger <restranger@disroot.org>

--- system/machine/pc/modules/hardware/graphics-card/default.nix
+++ system/machine/pc/modules/hardware/graphics-card/default.nix
@@ -8,21 +8,13 @@
       enable = true;
       enable32Bit = true;
       extraPackages = with pkgs; [
-        amdvlk
         mesa
         mesa.opencl
-      ];
-      extraPackages32 = with pkgs; [
-        driversi686Linux.amdvlk
       ];
     };
     amdgpu = {
       initrd.enable = true;
       opencl.enable = true;
-      amdvlk = {
-        enable = true;
-        support32Bit.enable = true;
-      };
     };
   };
   environment.variables = {
