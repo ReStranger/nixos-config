f7d193a367f31146821388f8475e55774ff7d841
ReStranger <restranger@disroot.org>
2025-10-21 20:46:54 +0300

    feat(treefmt): Improve config
    Signed-off-by: ReStranger <restranger@disroot.org>
    
    Co-authored-by: TheMaxMur <muravjev.mak@yandex.ru>

--- parts/treefmt/default.nix
+++ parts/treefmt/default.nix
@@ -12,12 +12,31 @@
     # For nix fmt
     treefmt.config = {
       projectRootFile = "flake.nix";
+      settings = {
+        global.excludes = [
+          "LICENSE"
+          ".gitattributes"
+
+          "*.png"
+          "*.svg"
+          "*.bp"
+          "*.conf"
+          "*.zsh"
+          "**/*rc"
+          "**/.gitkeep"
+
+          "secrets/**"
+        ];
+      };
 
       programs = {
         deadnix.enable = true;
         nixfmt.enable = true;
         statix.enable = true;
         terraform.enable = true;
+        shellcheck.enable = true;
+        stylua.enable = true;
+        yamlfmt.enable = true;
       };
     };
   };
