30fe7c7b701f49695390a01f0ad42e93f70d7cf3
ReStranger <restranger@disroot.org>
2025-11-07 23:26:55 +0300

    refactor(restranger/xdg): Moved program names to separate variables
    Signed-off-by: ReStranger <restranger@disroot.org>

--- home/users/restranger/modules/xdg/default.nix
+++ home/users/restranger/modules/xdg/default.nix
@@ -1,60 +1,140 @@
 {
   config,
   lib,
+  inputs,
+  pkgs,
   ...
 }:
-
 let
-  cfg = config.module.user.xdg;
   inherit (lib) mkEnableOption mkIf;
+
+  browser = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.beta.meta.desktopFileName;
+  editor = "nvim.desktop";
+  imageViewer = "imv.desktop";
+  videoPlayer = "mpv.desktop";
+  fileManager = "org.gnome.Nautilus.desktop";
+  pdfReader = "org.pwmt.zathura.desktop";
+  torrent = "transmission-gtk.desktop";
+  mail = "Thunderbird.desktop";
+  wine = "PortProton.desktop";
+
+  mimeBrowser = [
+    "application/x-extension-shtml"
+    "application/x-extension-xhtml"
+    "application/x-extension-html"
+    "application/x-extension-xht"
+    "application/x-extension-htm"
+    "x-scheme-handler/unknown"
+    "x-scheme-handler/mailto"
+    "x-scheme-handler/chrome"
+    "x-scheme-handler/about"
+    "x-scheme-handler/https"
+    "x-scheme-handler/http"
+    "application/xhtml+xml"
+    "application/json"
+    "text/html"
+  ];
+
+  mimeFileManager = [
+    "inode/directory"
+    "application/x-gnome-saved-search"
+  ];
+
+  mimeEditor = [
+    "text/markdown"
+    "text/plain"
+  ];
+
+  mimeImageViewer = [
+    "image/png"
+    "image/jpeg"
+    "image/jpg"
+    "image/svg+xml"
+  ];
+
+  mimeVideoPlayer = [
+    "image/gif"
+    "video/mp4"
+  ];
+
+  mimePdfReader = [
+    "application/pdf"
+  ];
+
+  mimeTelegram = [
+    "x-scheme-handler/tg"
+    "x-scheme-handler/tonsite"
+  ];
+
+  mimeObsidian = [
+    "x-scheme-handler/obsidian"
+  ];
+
+  mimeAnytype = [
+    "x-scheme-handler/anytype"
+  ];
+
+  mimeTorrent = [
+    "x-scheme-handler/magnet"
+  ];
+
+  mimeMail = [
+    "x-scheme-handler/mailto"
+    "message/rfc822"
+    "x-scheme-handler/mid"
+  ];
+
+  mimeWine = [
+    "application/vnd.microsoft.portable-executable"
+    "application/x-msi"
+  ];
+
+  mkAssociations =
+    names: desktopFile:
+    builtins.listToAttrs (
+      map (name: {
+        inherit name;
+        value = desktopFile;
+      }) names
+    );
+
+  browserApps = mkAssociations mimeBrowser browser;
+  fileManagerApps = mkAssociations mimeFileManager fileManager;
+  editorApps = mkAssociations mimeEditor editor;
+  imageViewerApps = mkAssociations mimeImageViewer imageViewer;
+  videoPlayerApps = mkAssociations mimeVideoPlayer videoPlayer;
+  pdfReaderApps = mkAssociations mimePdfReader pdfReader;
+
+  telegramApps = mkAssociations mimeTelegram "com.ayugram.desktop.desktop";
+  obsidianApps = mkAssociations mimeObsidian "obsidian.desktop";
+  anytypeApps = mkAssociations mimeAnytype "anytype.desktop";
+  transmissionApps = mkAssociations mimeTorrent torrent;
+  thunderbirdApps = mkAssociations mimeMail mail;
+  portprotonApps = mkAssociations mimeWine wine;
+
+  associations =
+    browserApps
+    // fileManagerApps
+    // editorApps
+    // imageViewerApps
+    // videoPlayerApps
+    // pdfReaderApps
+    // telegramApps
+    // obsidianApps
+    // anytypeApps
+    // transmissionApps
+    // thunderbirdApps
+    // portprotonApps;
+
 in
 {
-  options = {
-    module.user.xdg.enable = mkEnableOption "Enables xdg";
-  };
+  options.module.user.xdg.enable = mkEnableOption "Enable xdg";
 
-  config = mkIf cfg.enable {
+  config = mkIf config.module.user.xdg.enable {
     xdg.mimeApps = {
       enable = true;
-
-      defaultApplications = {
-
-        "inode/directory" = "org.gnome.Nautilus.desktop";
-        "application/x-gnome-saved-search" = "org.gnome.Nautilus.desktop";
-
-        "text/markdown" = "nvim.desktop";
-        "text/plain" = "nvim.desktop";
-
-        "text/html" = "zen-beta.desktop";
-        "x-scheme-handler/http" = "zen-beta.desktop";
-        "x-scheme-handler/https" = "zen-beta.desktop";
-        "x-scheme-handler/about" = "zen-beta.desktop";
-        "x-scheme-handler/unknown" = "zen-beta.desktop";
-
-        "image/png" = "imv.desktop";
-        "image/jpeg" = "imv.desktop";
-        "image/jpg" = "imv.desktop";
-        "image/svg+xml" = "imv.desktop";
-
-        "image/gif" = "mpv.desktop";
-        "video/mp4" = "mpv.desktop";
-
-        "application/pdf" = "org.pwmt.zathura.desktop";
-
-        "x-scheme-handler/tg" = "com.ayugram.desktop.desktop";
-        "x-scheme-handler/tonsite" = "com.ayugram.desktop.desktop";
-
-        "x-scheme-handler/obsidian" = "obsidian.desktop";
-        "x-scheme-handler/anytype" = "anytype.desktop";
-
-        "x-scheme-handler/magnet" = "userapp-transmission-gtk-UCJKV2.desktop";
-        "x-scheme-handler/mailto" = "userapp-Thunderbird-QT8EW2.desktop";
-        "message/rfc822" = "userapp-Thunderbird-QT8EW2.desktop";
-        "x-scheme-handler/mid" = "userapp-Thunderbird-QT8EW2.desktop";
-
-        "application/vnd.microsoft.portable-executable" = "PortProton.desktop";
-        "application/x-msi" = "PortProton.desktop";
-      };
+      defaultApplications = associations;
+      associations.added = associations;
     };
   };
 }
