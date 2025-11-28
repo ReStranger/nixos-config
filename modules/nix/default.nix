daf1225786ebd69676cabfd34d13313f719fc470
ReStranger <restranger@disroot.org>
2025-11-28 19:11:56 +0300

    refactor(nix): Add all substituters in trusted-substituters
    Signed-off-by: ReStranger <restranger@disroot.org>

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -37,41 +37,45 @@ in
     };
 
     # Nix package manager settings
-    nix = {
-      package = pkgs.lix;
-      registry.s.flake = inputs.self;
-
-      settings = {
-        experimental-features = [
-          "nix-command"
-          "flakes"
-        ];
-        auto-optimise-store = true;
-        builders-use-substitutes = true;
-
-        allowed-users = [ "@wheel" ];
-
-        trusted-users = [
-          "root"
-          username
-        ];
-
-        substituters = [
+    nix =
+      let
+        substituters-list = [
           "https://nyx.chaotic.cx"
           "https://hyprland.cachix.org"
           "https://wezterm.cachix.org"
           "https://anyrun.cachix.org"
         ];
+      in
+      {
+        package = pkgs.lix;
+        registry.s.flake = inputs.self;
 
-        trusted-substituters = [ "https://hyprland.cachix.org" ];
+        settings = {
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
 
-        trusted-public-keys = [
-          "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-          "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
-          "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-        ];
+          auto-optimise-store = true;
+          builders-use-substitutes = true;
+
+          allowed-users = [ "@wheel" ];
+
+          trusted-users = [
+            "root"
+            username
+          ];
+
+          substituters = substituters-list;
+          trusted-substituters = substituters-list;
+
+          trusted-public-keys = [
+            "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+            "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+            "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
+            "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+          ];
+        };
       };
-    };
   };
 }
