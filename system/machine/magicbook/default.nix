1210a599b052607180c34c2d3c89e6cb9870079c
ReStranger <restranger@disroot.org>
2025-12-03 20:53:48 +0300

    chore(magicbook): Add zerotier-one networks
    Signed-off-by: ReStranger <restranger@disroot.org>

--- system/machine/magicbook/default.nix
+++ system/machine/magicbook/default.nix
@@ -44,7 +44,13 @@ _: {
       scx.enable = true;
       systemd-oomd.enable = true;
       upower.enable = true;
-      zerotier-one.enable = true;
+      zerotier-one = {
+        enable = true;
+        joinNetworks = [
+          "8bd5124fd65dec01" # re_sshd
+          "af415e486f516107" # party
+        ];
+      };
       zram = {
         enable = true;
         deviceNumber = 2;
