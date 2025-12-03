9e5b291760a6c669abfda934f2e9969f2f58e0b9
ReStranger <restranger@disroot.org>
2025-12-03 11:27:54 +0300

    refactor(users): Reduced visibility of variables
    Signed-off-by: ReStranger <restranger@disroot.org>

--- system/nixos/modules/users/default.nix
+++ system/nixos/modules/users/default.nix
@@ -15,58 +15,62 @@ let
     mkIf
     genAttrs
     ;
-
-  userNames = [
-    "${username}"
-    "root"
-  ];
-
-  sopsFile = "${self}/secrets/system/${hostname}/secrets.yaml";
 in
 {
   options.module.users.enable = mkEnableOption "Enables users";
 
-  config = mkIf cfg.enable {
-    sops.secrets = genAttrs (map (u: "user_password/${u}") userNames) (_up: {
-      neededForUsers = true;
-      inherit sopsFile;
-    });
-
-    users = {
-      mutableUsers = false;
-
-      groups = {
-        "${username}" = {
-          gid = 1000;
-        };
-      };
-
-      users = genAttrs userNames (
-        u:
+  config =
+    let
+      userNames = [
+        "${username}"
+        "root"
+      ];
+    in
+    mkIf cfg.enable {
+      sops.secrets =
         let
-          common = {
-            shell = pkgs.zsh;
-            hashedPasswordFile = config.sops.secrets."user_password/${u}".path;
-          };
+          sopsFile = "${self}/secrets/system/${hostname}/secrets.yaml";
         in
-        if u == "${username}" then
-          common
-          // {
-            uid = 1000;
-            isNormalUser = true;
-            description = "${username}";
-            extraGroups = [
-              "audio"
-              "networkmanager"
-              "wheel"
-              "docker"
-              "libvirtd"
-              "dialout"
-            ];
-          }
-        else
-          common
-      );
+        genAttrs (map (u: "user_password/${u}") userNames) (_up: {
+          neededForUsers = true;
+          inherit sopsFile;
+        });
+
+      users = {
+        mutableUsers = false;
+
+        groups = {
+          "${username}" = {
+            gid = 1000;
+          };
+        };
+
+        users = genAttrs userNames (
+          u:
+          let
+            common = {
+              shell = pkgs.zsh;
+              hashedPasswordFile = config.sops.secrets."user_password/${u}".path;
+            };
+          in
+          if u == "${username}" then
+            common
+            // {
+              uid = 1000;
+              isNormalUser = true;
+              description = "${username}";
+              extraGroups = [
+                "audio"
+                "networkmanager"
+                "wheel"
+                "docker"
+                "libvirtd"
+                "dialout"
+              ];
+            }
+          else
+            common
+        );
+      };
     };
-  };
 }
