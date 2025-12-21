5ebd6e3eac5fc619904e77eaafd4a1a0393b7ef9
ReStranger <restranger@disroot.org>
2025-12-21 23:00:18 +0300

    feat(hyprland/variables): Now only wayland
    Signed-off-by: ReStranger <restranger@disroot.org>

--- home/modules/hyprland/variables/default.nix
+++ home/modules/hyprland/variables/default.nix
@@ -14,10 +14,10 @@ let
     XDG_SESSION_DESKTOP = "Hyprland";
 
     _JAVA_AWT_WM_NONREPARENTING = 1;
-    GDK_BACKEND = "wayland,x11";
-    SDL_VIDEODRIVER = "wayland,x11,windows";
+    GDK_BACKEND = "wayland";
+    SDL_VIDEODRIVER = "wayland";
     CLUTTER_BACKEND = "wayland";
-    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+    ELECTRON_OZONE_PLATFORM_HINT = "wayland";
     DESKTOPINTEGRATION = "1";
     MANGOHUD = "1";
 
@@ -25,7 +25,7 @@ let
     MOZ_DISABLE_RDD_SANDBOX = "1";
 
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    QT_QPA_PLATFORM = "wayland;xcb ";
+    QT_QPA_PLATFORM = "wayland";
     QT_QPA_PLATFORMTHEME = mkForce "gtk3";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
 
