ee939225f8dfda6d926db968728b94b1ed20f9df
ReStranger <restranger@disroot.org>
2026-02-09 16:28:54 +0300

    fix(magicbook/graphics-card): Fixed OpenCL setting
    Signed-off-by: ReStranger <restranger@disroot.org>

--- system/machine/magicbook/modules/hardware/graphics-card/default.nix
+++ system/machine/magicbook/modules/hardware/graphics-card/default.nix
@@ -7,9 +7,6 @@
     graphics = {
       enable = true;
       enable32Bit = true;
-      extraPackages = with pkgs; [
-        rocmPackages.clr.icd
-      ];
     };
     amdgpu = {
       initrd.enable = true;
