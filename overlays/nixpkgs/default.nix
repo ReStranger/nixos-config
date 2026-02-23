9d0ba95773f7cfa14c1cf4235b5fa4faf23216f6
ReStranger <restranger@disroot.org>
2026-02-23 17:24:40 +0300

    feat(overlays/nixpkgs): Add selfPkgs
    Signed-off-by: ReStranger <restranger@disroot.org>

--- overlays/nixpkgs/default.nix
+++ overlays/nixpkgs/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ self, inputs, ... }:
 let
   baseSettings = {
     config = {
@@ -16,34 +16,48 @@ let
 in
 {
   nixpkgs.overlays = [
-    (final: _prev: {
-      stable = import inputs.stable {
-        inherit (final.stdenv.hostPlatform) system;
-        inherit (baseSettings) config;
-      };
-      stable-unfree = import inputs.stable {
-        inherit (final.stdenv.hostPlatform) system;
-        inherit (unfreeSettings) config;
-      };
+    (
+      final: prev:
+      let
+        selfPkgs = prev.lib.packagesFromDirectoryRecursive {
+          inherit (final) callPackage;
+          directory = "${self}/pkgs";
+        };
 
-      unstable = import inputs.unstable {
-        inherit (final.stdenv.hostPlatform) system;
-        inherit (baseSettings) config;
-      };
-      unstable-unfree = import inputs.unstable {
-        inherit (final.stdenv.hostPlatform) system;
-        inherit (unfreeSettings) config;
-      };
+        selfPkgsUnwrapped = builtins.mapAttrs (
+          _name: value: if (builtins.isAttrs value && value ? default) then value.default else value
+        ) selfPkgs;
+      in
+      selfPkgsUnwrapped
+      // {
+        stable = import inputs.stable {
+          inherit (final.stdenv.hostPlatform) system;
+          inherit (baseSettings) config;
+        };
+        stable-unfree = import inputs.stable {
+          inherit (final.stdenv.hostPlatform) system;
+          inherit (unfreeSettings) config;
+        };
 
-      master = import inputs.master {
-        inherit (final.stdenv.hostPlatform) system;
-        inherit (baseSettings) config;
-      };
-      master-unfree = import inputs.master {
-        inherit (final.stdenv.hostPlatform) system;
-        inherit (unfreeSettings) config;
-      };
-    })
+        unstable = import inputs.unstable {
+          inherit (final.stdenv.hostPlatform) system;
+          inherit (baseSettings) config;
+        };
+        unstable-unfree = import inputs.unstable {
+          inherit (final.stdenv.hostPlatform) system;
+          inherit (unfreeSettings) config;
+        };
+
+        master = import inputs.master {
+          inherit (final.stdenv.hostPlatform) system;
+          inherit (baseSettings) config;
+        };
+        master-unfree = import inputs.master {
+          inherit (final.stdenv.hostPlatform) system;
+          inherit (unfreeSettings) config;
+        };
+      }
+    )
   ];
 
 }
