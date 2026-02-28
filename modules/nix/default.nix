d90f28778c725ab1d415082b33ef923515330089
ReStranger <restranger@disroot.org>
2026-02-28 12:17:56 +0300

    fix(nix): Add missed cache
    Signed-off-by: ReStranger <restranger@disroot.org>

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -57,6 +57,7 @@ in
         ];
 
         substituters = [
+          "https://cache.nixos.org"
           "https://hyprland.cachix.org"
           "https://wezterm.cachix.org"
           "https://anyrun.cachix.org"
@@ -65,6 +66,7 @@ in
         trusted-substituters = [ "https://hyprland.cachix.org" ];
 
         trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
           "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
