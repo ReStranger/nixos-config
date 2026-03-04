19dc1e5d0470204aa61feb4782e767fe95f63b2f
ReStranger <restranger@disroot.org>
2026-03-05 00:49:44 +0300

    feat(nix): Added own cache
    Signed-off-by: ReStranger <restranger@disroot.org>

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -54,6 +54,7 @@ in
         ];
 
         substituters = [
+          "https://re-cache.cachix.org"
           "https://hyprland.cachix.org"
           "https://wezterm.cachix.org"
           "https://anyrun.cachix.org"
@@ -62,6 +63,7 @@ in
         trusted-substituters = [ "https://hyprland.cachix.org" ];
 
         trusted-public-keys = [
+          "re-cache.cachix.org-1:zIzN9Bp2Lwpt5qMc5XReiFsgSx6G4+wZMy9UHCDJ4X4="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
           "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
