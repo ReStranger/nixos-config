4e58faf963322e44289c789b230e873642a99b37
ReStranger <restranger@disroot.org>
2026-03-05 22:21:10 +0300

    feat(magicbook/extra-hardware): Fix driver crach
    Signed-off-by: ReStranger <restranger@disroot.org>

--- system/machine/magicbook/modules/hardware/extra-hardware/default.nix
+++ system/machine/magicbook/modules/hardware/extra-hardware/default.nix
@@ -21,4 +21,34 @@
   # SUBSYSTEM=="pci", ATTR{power/control}="auto"
   # ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto"
   #   '';
+
+  systemd.services.ath11k-rmmod-before-sleep = {
+    description = "Unload ath11k_pci before sleep";
+    before = [ "sleep.target" ];
+    wantedBy = [ "sleep.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.kmod}/bin/rmmod ath11k_pci";
+    };
+  };
+
+  systemd.services.ath11k-modprobe-after-suspend = {
+    description = "Load ath11k_pci after suspend.target";
+    after = [
+      "suspend.target"
+      "suspend-then-hibernate.target"
+      "hibernate.target"
+      "hybrid-sleep.target"
+    ];
+    wantedBy = [
+      "suspend.target"
+      "suspend-then-hibernate.target"
+      "hibernate.target"
+      "hybrid-sleep.target"
+    ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.kmod}/bin/modprobe ath11k_pci";
+    };
+  };
 }
