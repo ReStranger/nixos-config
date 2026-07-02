f1ace8a76bdad6cb0bdccd7e2db361f202fe5b8e
ReStranger <restranger@disroot.org>
2026-07-03 01:01:49 +0300

    refactor(parts): Drop `builtins` from the `toString` function
    Signed-off-by: ReStranger <restranger@disroot.org>

--- parts/default.nix
+++ parts/default.nix
@@ -1,7 +1,7 @@
 {lib, ...}: {
   imports = builtins.filter (module: lib.pathIsDirectory module) (
-    map (module: builtins.toString ./. + "/${module}") (
-      builtins.attrNames (builtins.readDir (builtins.toString ./.))
+    map (module: toString ./. + "/${module}") (
+      builtins.attrNames (builtins.readDir (toString ./.))
     )
   );
 }
