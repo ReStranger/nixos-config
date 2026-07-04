bba211f2ce3fc211449fde8d550d1090c04f0294
ReStranger <restranger@disroot.org>
2026-07-05 02:33:36 +0300

    feat(qt): Add icon theme settings
    Signed-off-by: ReStranger <restranger@disroot.org>

--- home/modules/qt/default.nix
+++ home/modules/qt/default.nix
@@ -1,10 +1,26 @@
 {
   config,
   lib,
+  theme,
   ...
 }: let
   cfg = config.module.qt;
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) mkEnableOption mkIf mkForce;
+
+  iconTheme =
+    {
+      catppuccin-mocha = "Tela-circle-dracula-dark";
+      touka = "Papirus-Dark";
+    }.${
+      theme
+    } or "MoreWaita";
+
+  qtSettings = {
+    Appearance = {
+      icon_theme = iconTheme;
+      standard_dialogs = mkForce "xdgdesktopportal";
+    };
+  };
 in {
   options.module.qt = {
     enable = mkEnableOption "Enable qt module";
@@ -15,6 +31,8 @@ in {
       enable = true;
       platformTheme.name = "qtct";
       style.name = "kvantum";
+      qt5ctSettings = qtSettings;
+      qt6ctSettings = qtSettings;
     };
   };
 }
