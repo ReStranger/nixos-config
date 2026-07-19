5c03e5d066ae45c0e54b934b78fb06952606c40e
ReStranger <restranger@disroot.org>
2026-07-19 04:46:09 +0300

    feat(ollama): Add context option
    Signed-off-by: ReStranger <restranger@disroot.org>

--- system/nixos/modules/services/ollama/default.nix
+++ system/nixos/modules/services/ollama/default.nix
@@ -6,7 +6,7 @@
 }: let
   cfg = config.module.services.ollama;
   inherit (lib) mkEnableOption mkIf mkOption;
-  inherit (lib.types) package;
+  inherit (lib.types) package ints;
 in {
   options.module.services.ollama = {
     enable = mkEnableOption "Enable ollama daemon";
@@ -15,11 +15,19 @@ in {
       description = "Ollama package to use";
       default = pkgs.ollama;
     };
+    context = mkOption {
+      type = ints.positive;
+      description = "Maximum ollama context";
+      default = 131072;
+    };
   };
   config = mkIf cfg.enable {
     services.ollama = {
       enable = true;
       openFirewall = config.networking.firewall.enable;
+      environmentVariables = {
+        OLLAMA_CONTEXT_LENGTH = toString cfg.context;
+      };
       inherit (cfg) package;
     };
   };
