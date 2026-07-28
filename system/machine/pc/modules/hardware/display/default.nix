04352ee7c577d6b6999f7b305ce75e17f69ec952
ReStranger <restranger@disroot.org>
2026-07-28 06:26:14 +0300

    chore(pc/display): update display name
    Signed-off-by: ReStranger <restranger@disroot.org>

--- system/machine/pc/modules/hardware/display/default.nix
+++ system/machine/pc/modules/hardware/display/default.nix
@@ -4,54 +4,54 @@
     hyprland = {
       monitor = [
         {
-          output = "HDMI-A-1";
-          mode = "1920x1080@60";
+          output = "HDMI-A-2";
+          mode = "1920x1080@75";
           position = "0x0";
-          scale = "1.25";
+          scale = "1.0";
         }
       ];
 
       workspace_rule = [
         {
           workspace = 1;
-          monitor = "HDMI-A-1";
+          monitor = "HDMI-A-2";
           default = true;
         }
         {
           workspace = 2;
-          monitor = "HDMI-A-1";
+          monitor = "HDMI-A-2";
         }
         {
           workspace = 3;
-          monitor = "HDMI-A-1";
+          monitor = "HDMI-A-2";
         }
         {
           workspace = 4;
-          monitor = "HDMI-A-1";
+          monitor = "HDMI-A-2";
         }
         {
           workspace = 5;
-          monitor = "HDMI-A-1";
+          monitor = "HDMI-A-2";
         }
         {
           workspace = 6;
-          monitor = "HDMI-A-1";
+          monitor = "HDMI-A-2";
         }
         {
           workspace = 7;
-          monitor = "HDMI-A-1";
+          monitor = "HDMI-A-2";
         }
         {
           workspace = 8;
-          monitor = "HDMI-A-1";
+          monitor = "HDMI-A-2";
         }
         {
           workspace = 9;
-          monitor = "HDMI-A-1";
+          monitor = "HDMI-A-2";
         }
         {
           workspace = 0;
-          monitor = "HDMI-A-1";
+          monitor = "HDMI-A-2";
         }
       ];
     };
