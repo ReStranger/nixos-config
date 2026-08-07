29e03be1b108d58e85adac3e17c25ff2af146bd5
ReStranger <restranger@disroot.org>
2026-08-07 19:04:25 +0300

    refactor(magicbook/extra-hardware): made systemd services declaration more elegant
    Signed-off-by: ReStranger <restranger@disroot.org>

--- system/machine/magicbook/modules/hardware/extra-hardware/default.nix
+++ system/machine/magicbook/modules/hardware/extra-hardware/default.nix
@@ -18,33 +18,35 @@
   services.udev.extraRules = ''
     ACTION=="add|change", SUBSYSTEM=="pci", TEST=="power/control", ATTR{power/control}="auto"
   '';
-  systemd.services.ath11k-rmmod-before-sleep = {
-    description = "Unload ath11k_pci before sleep";
-    before = ["sleep.target"];
-    wantedBy = ["sleep.target"];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.kmod}/bin/rmmod ath11k_pci";
-    };
-  };
 
-  systemd.services.ath11k-modprobe-after-suspend = {
-    description = "Load ath11k_pci after suspend.target";
-    after = [
-      "suspend.target"
-      "suspend-then-hibernate.target"
-      "hibernate.target"
-      "hybrid-sleep.target"
-    ];
-    wantedBy = [
-      "suspend.target"
-      "suspend-then-hibernate.target"
-      "hibernate.target"
-      "hybrid-sleep.target"
-    ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.kmod}/bin/modprobe ath11k_pci";
+  systemd.services = {
+    ath11k-rmmod-before-sleep = {
+      description = "Unload ath11k_pci before sleep";
+      before = ["sleep.target"];
+      wantedBy = ["sleep.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.kmod}/bin/rmmod ath11k_pci";
+      };
+    };
+    ath11k-modprobe-after-suspend = {
+      description = "Load ath11k_pci after suspend.target";
+      after = [
+        "suspend.target"
+        "suspend-then-hibernate.target"
+        "hibernate.target"
+        "hybrid-sleep.target"
+      ];
+      wantedBy = [
+        "suspend.target"
+        "suspend-then-hibernate.target"
+        "hibernate.target"
+        "hybrid-sleep.target"
+      ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.kmod}/bin/modprobe ath11k_pci";
+      };
     };
   };
 }
