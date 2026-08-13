096ede7f2f592ec02debf5d712847472ae84af93
ReStranger <restranger@disroot.org>
2026-08-13 20:41:53 +0300

    chore(tmux): trim extra config strings
    Signed-off-by: ReStranger <restranger@disroot.org>

--- home/modules/tmux/default.nix
+++ home/modules/tmux/default.nix
@@ -32,53 +32,51 @@ in {
       keyMode = "vi";
       shortcut = "a";
       extraConfig = ''
-              set-option -ga terminal-overrides ",$TERM:Tc"
-              set -g default-terminal "$TERM"
+        set-option -ga terminal-overrides ",$TERM:Tc"
+        set -g default-terminal "$TERM"
 
-              set -gq allow-passthrough on
-              set -g visual-activity off
-              set -g extended-keys on
-              set -g extended-keys-format csi-u
+        set -gq allow-passthrough on
+        set -g visual-activity off
+        set -g extended-keys on
+        set -g extended-keys-format csi-u
 
-              # Сортировка по имени
-              bind s choose-tree -sZ -O name
+        bind s choose-tree -sZ -O name
 
-              # Переназначение клавиш
-              unbind %
-              bind b split-window -h
+        unbind %
+        bind b split-window -h
 
-              unbind '"'
-              bind v split-window -v
+        unbind '"'
+        bind v split-window -v
 
-              bind -r j resize-pane -D 5
-              bind -r k resize-pane -U 5
-              bind -r l resize-pane -R 5
-              bind -r h resize-pane -L 5
+        bind -r j resize-pane -D 5
+        bind -r k resize-pane -U 5
+        bind -r l resize-pane -R 5
+        bind -r h resize-pane -L 5
 
-              bind -r m resize-pane -Z
+        bind -r m resize-pane -Z
 
-              is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
-            | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
-              bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
-              bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
-              bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
-              bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
-              tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
+        is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+        | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+        bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
+        bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
+        bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
+        bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
+        tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
         if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
             "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
         if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
             "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
-              bind-key -T copy-mode-vi 'C-h' select-pane -L
-              bind-key -T copy-mode-vi 'C-j' select-pane -D
-              bind-key -T copy-mode-vi 'C-k' select-pane -U
-              bind-key -T copy-mode-vi 'C-l' select-pane -R
-              bind-key -T copy-mode-vi 'C-\' select-pane -l
-              bind-key -T copy-mode-vi 'v' send -X begin-selection
-              bind-key -T copy-mode-vi 'y' send -X copy-selection
+        bind-key -T copy-mode-vi 'C-h' select-pane -L
+        bind-key -T copy-mode-vi 'C-j' select-pane -D
+        bind-key -T copy-mode-vi 'C-k' select-pane -U
+        bind-key -T copy-mode-vi 'C-l' select-pane -R
+        bind-key -T copy-mode-vi 'C-\' select-pane -l
+        bind-key -T copy-mode-vi 'v' send -X begin-selection
+        bind-key -T copy-mode-vi 'y' send -X copy-selection
 
-              unbind -T copy-mode-vi MouseDragEnd1Pane
+        unbind -T copy-mode-vi MouseDragEnd1Pane
 
-              set -g @resurrect-capture-pane-contents 'on'
+        set -g @resurrect-capture-pane-contents 'on'
       '';
     };
     xdg.configFile."tmux/tmux-nerd-font-window-name.yml".source = (pkgs.formats.yaml {}).generate "tmux-nerd-font-window-name.yml" {
