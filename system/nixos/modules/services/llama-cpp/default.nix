2480271d2a79882a75824e85d881e967eb793831
ReStranger <restranger@disroot.org>
2026-09-12 04:02:20 +0300

    refactor(llama-cpp): move model-specific settings to ini config
    Signed-off-by: ReStranger <restranger@disroot.org>

--- system/nixos/modules/services/llama-cpp/default.nix
+++ system/nixos/modules/services/llama-cpp/default.nix
@@ -5,8 +5,8 @@
   ...
 }: let
   cfg = config.module.services.llama-cpp;
-  inherit (lib) mkEnableOption mkIf mkOption optionalAttrs;
-  inherit (lib.types) package nullOr ints port str attrs;
+  inherit (lib) mkEnableOption mkIf mkOption;
+  inherit (lib.types) anything attrsOf ints package port str;
 in {
   options.module.services.llama-cpp = {
     enable = mkEnableOption "Enable llama.cpp inference server";
@@ -17,10 +17,10 @@ in {
       default = pkgs.llama-cpp;
     };
 
-    modelAlias = mkOption {
-      type = nullOr str;
-      description = "Path to a model directory exposed through llama-server aliases";
-      default = null;
+    host = mkOption {
+      type = str;
+      description = "IP address to listen on";
+      default = "0.0.0.0";
     };
 
     port = mkOption {
@@ -28,39 +28,14 @@ in {
       description = "HTTP server port";
       default = 11435;
     };
-
-    host = mkOption {
-      type = str;
-      description = "IP address to listen on";
-      default = "0.0.0.0";
-    };
-
-    contextSize = mkOption {
+    parallel = mkOption {
       type = ints.positive;
-      description = "Context size (KV cache)";
-      default = 131072;
-    };
-
-    gpuLayers = mkOption {
-      type = ints.positive;
-      description = "Number of layers to offload to GPU";
-      default = 99;
-    };
-
-    threads = mkOption {
-      type = ints.positive;
-      description = "Number of CPU threads for inference";
-      default = 6;
-    };
-
-    cmoeThreads = mkOption {
-      type = nullOr ints.positive;
-      description = "Number of MoE CPU expert threads";
-      default = null;
+      description = "Number of parallel request slots exposed by llama-server";
+      default = 1;
     };
 
     extraSettings = mkOption {
-      type = attrs;
+      type = attrsOf anything;
       description = "Extra settings passed to llama-server (see services.llama-cpp.settings)";
       default = {};
     };
@@ -70,21 +45,12 @@ in {
     services.llama-cpp = {
       enable = true;
       inherit (cfg) package;
-      openFirewall = true;
+      openFirewall = config.networking.firewall.enable;
       settings =
         {
           inherit (cfg) host;
           inherit (cfg) port;
-          n-gpu-layers = cfg.gpuLayers;
-          ctx-size = cfg.contextSize;
-          inherit (cfg) threads;
-          parallel = 1;
-        }
-        // optionalAttrs (cfg.cmoeThreads != null) {
-          n-cpu-moe = cfg.cmoeThreads;
-        }
-        // optionalAttrs (cfg.modelAlias != null) {
-          model-alias = cfg.modelAlias;
+          inherit (cfg) parallel;
         }
         // cfg.extraSettings;
     };
