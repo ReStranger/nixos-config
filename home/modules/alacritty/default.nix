aa71346fb7596495e64e80e3e7c782e3c0550ccc
ReStranger <restranger@disroot.org>
2026-09-17 19:46:20 +0300

    refactor(alacritty): made module more elegant
    Signed-off-by: ReStranger <restranger@disroot.org>

--- home/modules/alacritty/default.nix
+++ home/modules/alacritty/default.nix
@@ -1,6 +1,5 @@
 {
   config,
-  hostname,
   lib,
   ...
 }: let
@@ -9,18 +8,16 @@
     (lib)
     mkEnableOption
     mkOption
+    mkForce
     mkIf
-    types
     ;
+  inherit (lib.types) str;
 in {
   options.module.alacritty = {
     enable = mkEnableOption "Enable alacritty module";
     font = mkOption {
-      type = types.enum [
-        "JetBrains Mono Nerd Font"
-        "Operator Mono"
-      ];
-      default = "Operator Mono";
+      type = str;
+      default = "Maple Mono NF";
       description = ''
         Font for alacritty module
       '';
@@ -31,70 +28,35 @@ in {
     programs.alacritty = {
       enable = true;
       settings = {
-        live_config_reload = true;
+        general.live_config_reload = true;
         env = {
           TERM = "xterm-256color";
           WINIT_X11_SCALE_FACTOR = "1.0";
         };
 
         window = {
-          opacity = 0.87;
+          opacity = mkForce 0.87;
           blur = true;
         };
 
-        font =
-          if (cfg.font == "JetBrains Mono Nerd Font")
-          then {
-            normal = {
-              family = "JetBrains Mono Nerd Font";
-              style = "Regular";
-            };
-            bold = {
-              family = "JetBrains Mono Nerd Font";
-              style = "Bold";
-            };
-            italic = {
-              family = "JetBrains Mono Nerd Font";
-              style = "Italic";
-            };
-            bold_italic = {
-              family = "JetBrains Mono Nerd Font";
-              style = "BoldItalic";
-            };
-            size =
-              if (hostname == "pc")
-              then 10
-              else if (hostname == "magicbook")
-              then 11
-              else 10;
-          }
-          else if (cfg.font == "Operator Mono")
-          then {
-            normal = {
-              family = "Operator Mono";
-              style = "Regular";
-            };
-            bold = {
-              family = "Operator Mono";
-              style = "Bold";
-            };
-            italic = {
-              family = "Operator Mono";
-              style = "Italic";
-            };
-            bold_italic = {
-              family = "Operator Mono";
-              style = "BoldItalic";
-            };
-            size =
-              if (hostname == "pc")
-              then 11
-              else if (hostname == "magicbook")
-              then 13
-              else 11;
-          }
-          else {};
-        builtin_box_drawing = true;
+        font = {
+          normal = {
+            family = mkForce "${cfg.font}";
+            style = "Regular";
+          };
+          bold = {
+            family = mkForce "${cfg.font}";
+            style = "Bold";
+          };
+          italic = {
+            family = mkForce "${cfg.font}";
+            style = "Italic";
+          };
+          bold_italic = {
+            family = mkForce "${cfg.font}";
+            style = "BoldItalic";
+          };
+        };
       };
     };
   };
