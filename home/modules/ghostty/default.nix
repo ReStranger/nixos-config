6aac040c2a1f62f2b86d87332118a7a8cdca8c6d
ReStranger <restranger@disroot.org>
2026-09-20 04:59:49 +0300

    feat(ghostty): set background opacity to 87
    Signed-off-by: ReStranger <restranger@disroot.org>

--- home/modules/ghostty/default.nix
+++ home/modules/ghostty/default.nix
@@ -23,7 +23,7 @@ in {
       package = inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default;
       settings = {
         font-family = mkForce "Maple Mono NF";
-        background-opacity = mkForce 0.79;
+        background-opacity = mkForce 0.87;
         background-blur = true;
         window-inherit-working-directory = false;
         custom-shader = "cursor_smear.glsl";
