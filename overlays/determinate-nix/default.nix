769ae20cb0eb9d15c3a33de3d7d57e5ba16d96d1
ReStranger <restranger@disroot.org>
2026-09-23 21:06:12 +0300

    feat(determinate-nix): sync nix tools with config.nix.package
    Signed-off-by: ReStranger <restranger@disroot.org>

--- overlays/determinate-nix/default.nix
+++ overlays/determinate-nix/default.nix
@@ -1,10 +1,23 @@
 {config, ...}: {
   nixpkgs.overlays = [
-    (_final: prev: {
-      nix-init = prev.nix-init.override {nix = config.nix.package;};
+    (final: prev: {
+      nurl = prev.nurl.override {nix = config.nix.package;};
+      nix-init = prev.nix-init.override {
+        nix = config.nix.package;
+        inherit (final) nurl;
+      };
       nixpkgs-review = prev.nixpkgs-review.override {nix = config.nix.package;};
-      nix-update = prev.nix-update.override {nix = config.nix.package;};
+      nix-update = prev.nix-update.override {
+        nix = config.nix.package;
+        inherit (final) nixpkgs-review;
+      };
       nix-direnv = prev.nix-direnv.override {nix = config.nix.package;};
+      nixos-anywhere = prev.nixos-anywhere.override {nix = config.nix.package;};
+      nixos-rebuild-ng = prev.nixos-rebuild-ng.override {nix = config.nix.package;};
+      colmena = prev.colmena.override {
+        nix = config.nix.package;
+        inherit (prev) nix-eval-jobs;
+      };
     })
   ];
 }
